882b170091b48e00ffa18addf847a66ffc080ea5
orlamee <meetmichaelakindele@gmail.com>
2022-11-09 16:49:39 +0100

    form

--- src/components/login.js
+++ src/components/login.js
@@ -6,10 +6,19 @@ function Login() {
     <section className='login-section'>
       <div className='container'>
         <div className='row'>
-          <div className='col-md-6'>
-            <div className='left text-center'>
+          <div className='col-md-6 mb-2'>
+            <div className='left px-4 text-center'>
               <img src={login} alt="" className='img-fluid' />
-              <h2 className='my-3'>The future of savings & Investment</h2>
+              <h2 className='my-5'>The future of savings & Investment</h2>
+              <h6>Saving money typically means it is available when we need it<br/>and it has a low risk of losing value.</h6>
+            </div>
+          </div>
+          <div className='col-md-6'>
+            <div className='right px-4'>
+              <div className='login-form'>
+                <h3>Welcome!</h3>
+                <h6>Here’s how to Log in to access your account</h6>
+              </div>
             </div>
           </div>
         </div>
