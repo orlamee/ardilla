140e6b54c1e6f27dd535ffc406dc33bb9120fc15
orlamee <meetmichaelakindele@gmail.com>
2022-11-10 16:03:03 +0100

    fixes

--- src/components/login.js
+++ src/components/login.js
@@ -41,7 +41,11 @@ function Login() {
                     <label className="form-label"><i className="bi bi-shield-lock-fill me-2"></i>  Password</label>
                     <input type="password" className="form-control custom-login"/>
                   </div>
-                  <div className="form-group"><label className="form-check-label remember"><input type="checkbox" className="form-check-input" />  Remember me</label><a className="float-end" href="#">Forgot Password?</a></div>
+                  <div className='mobile'>
+                    <div className='mb-2'><label className="form-check-label remember"><input type="checkbox" className="form-check-input" />  Remember me</label></div>
+                    <div><a className="" href="#">Forgot Password?</a></div>
+                  </div>
+                  <div className="form-group desktop"><label className="form-check-label remember"><input type="checkbox" className="form-check-input" />  Remember me</label><a className="float-end" href="#">Forgot Password?</a></div>
                   <div className="mt-4 mb-5">
                     <button type="button" className="btn btn-outline-primary px-5 py-3 ardilla-btn" style={{width: "100%"}}>Log In</button>
                   </div>
