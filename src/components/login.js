9800d1d1a7d8f1d3bf1e6bfdaf1debe92879e51d
orlamee <meetmichaelakindele@gmail.com>
2022-11-10 10:59:35 +0100

    login

--- src/components/login.js
+++ src/components/login.js
@@ -12,13 +12,12 @@ function Login() {
               <img src={login} alt="" className='img-fluid' />
               <h2 className='my-4'>The future of savings & Investment</h2>
               <h6 className='mb-4'>Saving money typically means it is available when we need it<br/>and it has a low risk of losing value.</h6>
-              <div><Link className='btn btn-outline-primary px-5 py-3 ardilla-btn' to="/">Create account</Link></div>
 
 
-              {/* <div className='d-flex mb-3'>
-                <div><p className='text-center'>New user?</p></div>
+              <div className='d-flex justify-content-center mb-3'>
+                <div className='mt-3 me-2'><p className='text-center users'>New user?</p></div>
                 <div><Link className='btn btn-outline-primary px-5 py-3 ardilla-btn' to="/">Create account</Link></div>
-              </div> */}
+              </div>
             </div>
           </div>
           <div className='col-md-6'>
