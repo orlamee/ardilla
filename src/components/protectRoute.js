444b347b6e88ab9c2d4d30f6098e122eb1a2aec4
backend dev <developer@leapsail.com.ng>
2022-11-15 08:34:33 -0800

    check conit

--- src/components/protectRoute.js
+++ src/components/protectRoute.js
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  let user = true;
+  let user = false;
   // JSON.parse(localStorage.getItem("user"));
 
   if (!user) {
