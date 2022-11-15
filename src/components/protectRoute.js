55fd9b19d50475dca672d0dbf7e5d0032aaa34a3
backend dev <developer@leapsail.com.ng>
2022-11-15 09:49:52 -0800

    pw commit 23

--- src/components/protectRoute.js
+++ src/components/protectRoute.js
@@ -1,8 +1,7 @@
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  let user = false;
-  // JSON.parse(localStorage.getItem("user"));
+  const user = JSON.parse(sessionStorage.getItem("user"));
 
   if (!user) {
     return <Navigate to={"/sign-up"} />;
