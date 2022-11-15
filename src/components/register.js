018b4e174699911dc6a96c3324ccfae232bfe1c8
backend dev <developer@leapsail.com.ng>
2022-11-15 13:34:14 -0800

    kku 1234

--- src/components/register.js
+++ src/components/register.js
@@ -3,7 +3,7 @@ import logo from "../img/logo.svg";
 import testimonial from "../img/testimonial.svg";
 import register from "../img/code.svg";
 import dot from "../img/dot.svg";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import axios from "axios";
 import Swal from "sweetalert2";
 
@@ -11,7 +11,8 @@ function Register() {
   const [email, setEmail] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
+  // , useNavigate
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -27,10 +28,10 @@ function Register() {
 
       console.log(data);
 
-      sessionStorage.setItem(
-        "user",
-        JSON.stringify({ id: data.data._id, Bearer: data.token })
-      );
+      // sessionStorage.setItem(
+      //   "user",
+      //   JSON.stringify({ id: data.data._id, Bearer: data.token })
+      // );
 
       // navigate("/otp");
     } catch (error) {
