3e90195a1c9041227a48c21a50b9ebdd638278c8
orlamee <meetmichaelakindele@gmail.com>
2022-11-16 12:15:23 +0100

    products

--- src/components/landingpage/product.js
+++ src/components/landingpage/product.js
@@ -1,5 +1,6 @@
 import React from "react";
 import features from "../../img/products.svg";
+import blank from "../../img/blank.svg";
 
 function Products() {
   return(
@@ -9,8 +10,15 @@ function Products() {
           <h2>Products</h2>
         </div>
         <div className="row mt-5">
-          <div className="col-md-6 text-start">
+          <div className="col-md-4 text-start">
             <img src={features} alt="" className="img-fluid" />
+          </div>
+          <div className="col-md-4">
+            <img src={blank} alt="" className="img-fluid" />
+          </div>
+          <div className="col-md-4 text-start">
+            <h3>Save at your own<br/>pace!</h3>
+            <h6 className="my-4">Save your money daily, weekly, or monthly. No pressure. We work at your pace.</h6>
           </div>
         </div>
       </div>
