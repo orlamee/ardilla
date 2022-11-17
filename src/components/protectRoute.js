1071cd8f704b98f9f98ab5da1c8b9c85d8af5449
backend dev <developer@leapsail.com.ng>
2022-11-17 14:36:06 -0800

    th cmt 234

--- src/components/protectRoute.js
+++ src/components/protectRoute.js
@@ -1,7 +1,8 @@
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  const user = JSON.parse(sessionStorage.getItem("user"));
+  const user = true;
+  // JSON.parse(sessionStorage.getItem("user"));
 
   if (!user) {
     return <Navigate to={"/sign-up"} />;
