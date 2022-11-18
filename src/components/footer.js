7bfe1b38b79e64bc5ee2e79854ed1b78295cfbb0
orlamee <meetmichaelakindele@gmail.com>
2022-11-18 10:50:25 +0100

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -15,7 +15,7 @@ function Footer() {
                 <h2>Download for free<br/>Start saving Today</h2>
                 <form className="mt-5">
                   <div className="input-group">
-                    <input type="text" className="form-control input-lg download" placeholder="Your email"/>
+                    <input type="email" className="form-control input-lg download" placeholder="Your email"/>
                     <span className="input-group-btn" style={{marginLeft: "-9px"}}>
                       <Link className="btn download-btn btn-lg">Download for free</Link>
                     </span>
@@ -32,8 +32,34 @@ function Footer() {
       <footer className="footer-bottom">
         <div className="container">
           <div className="row">
-            <div className="col-md-3">
+            <div className="col-md-3 mb-3">
               <img src={logo} alt="" className="img-fluid" />
+              <p className="mt-4">Ardilla is a savings and investment platform that helps customers in achieving your financial goals.</p>
+            </div>
+            <div className="col-md-9">
+              <div className="row">
+                <div className="col-sm mb-3">
+                  <h4>Products</h4>
+                  <ul className="list-unstyled links">
+                    <li>Savings</li>
+                    <li>Investment</li>
+                    <li>Education</li>
+                    <li>Insurance</li>
+                  </ul>
+                </div>
+                <div className="col-sm mb-3">
+                  <h4>Company</h4>
+                </div>
+                <div className="col-sm mb-3">
+                  <h4>Transparency</h4>
+                </div>
+                <div className="col-sm mb-3">
+                  <h4>Developer</h4>
+                </div>
+                <div className="col-sm mb-3">
+                  <h4>Address</h4>
+                </div>
+              </div>
             </div>
           </div>
         </div>
