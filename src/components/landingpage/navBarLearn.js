f48efcec9d5565526311c73f6a6421543001d50d
orlamee <meetmichaelakindele@gmail.com>
2022-11-18 13:04:09 +0100

    Learn

--- src/components/landingpage/navBarLearn.js
+++ src/components/landingpage/navBarLearn.js
@@ -7,9 +7,9 @@ function LearnHeader() {
   return(
     <nav className="navbar fixed-top navbar-expand-lg navbar-light py-2 px-1">
       <div className='container'>
-        <Link className='navbar-brand' to="/learn">
+        <a className='navbar-brand' href="/learn">
           <img src={logo} alt="" className='img-fluid' style={{marginTop: "-8px"}} />
-        </Link>
+        </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown">
           <span className="navbar-toggler-icon"></span>
         </button>
