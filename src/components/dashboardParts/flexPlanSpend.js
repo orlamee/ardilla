3574b6bac611386108a91882cb453bc1cac14c91
overlord <rexatuzie@gmail.com>
2022-12-07 11:36:11 +0100

    wed commit 2

--- src/components/dashboardParts/flexPlanSpend.js
+++ src/components/dashboardParts/flexPlanSpend.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import home from "../../img/dashboard/home.svg";
 import portfolio from "../../img/dashboard/portfolio.svg";
 import investment from "../../img/dashboard/growth.svg";
@@ -11,11 +11,98 @@ import insurance from "../../img/dashboard/insurance.svg";
 import logout from "../../img/dashboard/logout.svg";
 import contact from "../../img/dashboard/contact.svg";
 import chat from "../../img/dashboard/chat.svg";
-
+import axios from "axios";
 
 function FlexPlanSpend() {
+  const [exp, setExp] = useState();
+  const [loading, setLoading] = useState(false);
+  const [msg, setMsg] = useState("");
+  const [err, setErr] = useState(false);
+  const [onSuccess, setOnSuccess] = useState(false);
+
+  const navigate = useNavigate();
+
+  const location = useLocation();
+
+  const ern = parseInt(location.state);
+
+  let user = JSON.parse(sessionStorage.getItem("user"));
+
+  const handleExp = async () => {
+    setLoading(true);
+
+    if (exp) {
+      let flexPlanObj = {
+        ern,
+        exp,
+      };
+
+      try {
+        const { data } = await axios.put(
+          `https://ardilla.herokuapp.com/ardilla/api/account/auto-flex-plan/${user._id}`,
+          { flexPlanObj }
+        );
+
+        setLoading(false);
+        const fp = data.data.flexPlan;
+        navigate("/flex-type", { state: fp });
+      } catch (error) {
+        setLoading(false);
+        setErr(true);
+        setMsg(`${error.response.data.msg || "Network error"} `);
+      }
+    } else {
+      setLoading(false);
+      setErr(true);
+      setMsg("choose a plan first");
+    }
+  };
+
+  const handleClickSuccess = () => {
+    setOnSuccess(false);
+  };
   return (
     <section className="main-dash">
+      {err && (
+        <div className="row justify-content-center">
+          <div className="col-md-6">
+            <div
+              className="alert alert-danger alert-dismissible fade show text-center text-danger"
+              role="alert"
+            >
+              <i className="bi bi-exclamation-circle me-3"></i>
+              {msg}
+              <button
+                type="button"
+                className="btn-close"
+                // data-bs-dismiss="alert"
+                onClick={() => setErr(false)}
+                aria-label="Close"
+              ></button>
+            </div>
+          </div>
+        </div>
+      )}
+      {onSuccess && (
+        <div className="row justify-content-center mt-5">
+          <div className="col-md-6">
+            <div
+              className="alert alert-success alert-dismissible fade show text-center text-success"
+              role="alert"
+            >
+              <i className="bi bi-patch-check-fill me-3"></i>
+              {msg}
+              <button
+                type="button"
+                className="btn-close"
+                // data-bs-dismiss="alert"
+                onClick={handleClickSuccess}
+                aria-label="Close"
+              ></button>
+            </div>
+          </div>
+        </div>
+      )}
       <div className="sidebar">
         <Link to="/dashboard" className="">
           <div className="d-flex flex-row">
@@ -30,7 +117,7 @@ function FlexPlanSpend() {
           </div>
         </Link>
         <Link to="/savings" className="active">
-          <div className="d-flex flex-row" >
+          <div className="d-flex flex-row">
             <img src={saving} alt="" className="img-fluid me-2 icons" />
             Savings
           </div>
@@ -89,44 +176,131 @@ function FlexPlanSpend() {
       <div className="content py-5 px-5 earning-section step-two">
         <div className="row earning">
           <div className="col-md-6">
-            <h2>Cadet {"<"}Starboy{"/>"},</h2>
+            <h2>
+              Cadet {"<"}Starboy{"/>"},
+            </h2>
           </div>
         </div>
         <div className="row justify-content-center earns">
           <div className="col-md-8 text-center">
-            <h3>How much do you<br/><span style={{color: "#E8356D"}}>spend</span> monthly</h3>
-            <p className="my-5">Enter a name you want to give your target plan</p>
+            <h3>
+              How much do you
+              <br />
+              <span style={{ color: "#E8356D" }}>spend</span> monthly
+            </h3>
+            <p className="my-5">
+              Enter a name you want to give your target plan
+            </p>
 
             <div className="mb-3">
-              <div className="btn-group me-3" role="group" aria-label="First group">
-                <button type="button" className="btn btn-flex">{"<"}100k</button>
-                
+              <div
+                className="btn-group me-3"
+                role="group"
+                aria-label="First group"
+              >
+                <button
+                  type="button"
+                  className="btn btn-flex"
+                  value={50000}
+                  onClick={(e) => setExp(e.target.value)}
+                >
+                  {"<"}100k
+                </button>
               </div>
-              <div className="btn-group me-3" role="group" aria-label="Second group">
-                <button type="button" className="btn btn-flex">101k - 250k</button>
-                
+              <div
+                className="btn-group me-3"
+                role="group"
+                aria-label="Second group"
+              >
+                <button
+                  type="button"
+                  className="btn btn-flex"
+                  value={100000}
+                  onClick={(e) => setExp(parseInt(e.target.value))}
+                >
+                  101k - 250k
+                </button>
               </div>
               <div className="btn-group" role="group" aria-label="Third group">
-                <button type="button" className="btn btn-flex">251k - 500k</button>
+                <button
+                  type="button"
+                  className="btn btn-flex"
+                  value={250000}
+                  onClick={(e) => setExp(parseInt(e.target.value))}
+                >
+                  251k - 500k
+                </button>
               </div>
             </div>
             <div className="mb-3">
-              <div className="btn-group me-3" role="group" aria-label="First group">
-                <button type="button" className="btn btn-flex">501k - 1M</button>
+              <div
+                className="btn-group me-3"
+                role="group"
+                aria-label="First group"
+              >
+                <button
+                  type="button"
+                  className="btn btn-flex"
+                  value={500000}
+                  onClick={(e) => setExp(parseInt(e.target.value))}
+                >
+                  501k - 1M
+                </button>
               </div>
-              <div className="btn-group me-3" role="group" aria-label="Second group">
-                <button type="button" className="btn btn-flex">1M - 5M</button>
+              <div
+                className="btn-group me-3"
+                role="group"
+                aria-label="Second group"
+              >
+                <button
+                  type="button"
+                  className="btn btn-flex"
+                  value={1000000}
+                  onClick={(e) => setExp(parseInt(e.target.value))}
+                >
+                  1M - 5M
+                </button>
               </div>
               <div className="btn-group" role="group" aria-label="Third group">
-                <button type="button" className="btn btn-flex">5M {">"}</button>
+                <button
+                  type="button"
+                  className="btn btn-flex"
+                  value={5000000}
+                  onClick={(e) => setExp(parseInt(e.target.value))}
+                >
+                  5M {">"}
+                </button>
               </div>
             </div>
             <div className="mb-3">
-              <div className="btn-group me-3" role="group" aria-label="First group">
-                <button type="button" className="btn btn-flex">Enter Amount</button>
+              <div
+                className="btn-group me-3"
+                role="group"
+                aria-label="First group"
+              >
+                <button type="button" className="btn btn-flex">
+                  Enter Amount
+                </button>
               </div>
             </div>
-            <div><Link className="btn btn-outline-primary px-5 py-3 ardilla-btn fs-6 mt-5" to="/flex-type" style={{width: "50%"}}>Continue</Link></div>
+            <div>
+              {loading ? (
+                <button
+                  className="btn btn-outline-primary px-5 py-3 ardilla-btn fs-6 mt-5"
+                  style={{ width: "50%" }}
+                >
+                  Loading
+                </button>
+              ) : (
+                <button
+                  className="btn btn-outline-primary px-5 py-3 ardilla-btn fs-6 mt-5"
+                  style={{ width: "50%" }}
+                  onClick={handleExp}
+                >
+                  Continue
+                </button>
+              )}
+            </div>
           </div>
         </div>
       </div>
@@ -135,4 +309,3 @@ function FlexPlanSpend() {
 }
 
 export default FlexPlanSpend;
- 
