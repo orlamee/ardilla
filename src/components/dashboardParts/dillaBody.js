d48dedeb85d58e694202553960c9510c304ea524
orlamee <meetmichaelakindele@gmail.com>
2022-12-13 15:02:42 +0100

    target-dilla

--- src/components/dashboardParts/dillaBody.js
+++ src/components/dashboardParts/dillaBody.js
@@ -40,6 +40,7 @@ import savingsplan from "../../img/dashboard/savingsplan.svg";
 import investmentplan from "../../img/dashboard/investmentplan.svg";
 import insuranceplan from "../../img/dashboard/insuranceplan.svg";
 import sanlogo from "../../img/dashboard/san-new.png";
+import becca from "../../img/dashboard/becca.svg";
 
 
 
@@ -691,7 +692,7 @@ function DillaBody() {
                             </div>
                             <i className="bi bi-chevron-right float-end" style={{marginTop: "-29px"}}></i>
                           </Link>
-                          <Link className="border-bottom pb-3 mb-3">
+                          <Link className="border-bottom pb-3 mb-3" data-bs-toggle="modal" data-bs-target="#dilla-send-user" type="button">
                             <div className="d-flex flex-row">
                               <img src={dillauser} alt="" className="img-fluid" />
                               <div className="ms-2 mt-2">
@@ -701,7 +702,7 @@ function DillaBody() {
                             </div>
                             <i className="bi bi-chevron-right float-end" style={{marginTop: "-29px"}}></i>
                           </Link>
-                          <Link className="border-bottom pb-3 mb-3">
+                          <Link className="border-bottom pb-3 mb-3" data-bs-toggle="modal" data-bs-target="#dilla-send-saving-plan" type="button">
                             <div className="d-flex flex-row">
                               <img src={savingsplan} alt="" className="img-fluid" />
                               <div className="ms-2 mt-2">
@@ -833,6 +834,235 @@ function DillaBody() {
                             <img src={sent} alt="" className="img-fluid" />
                             <h3 className="my-3">Money Transfered</h3>
                             <p>You have successfully sent <span style={{color: "#3D0072"}}>NGN50000 to <br/>SAN</span></p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* Send Money to User */}
+              <div className="modal flex-modal fade" id="dilla-send-user" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog right-dialog">
+                  <div className="modal-content right-content">
+                    <div className="modal-header">
+                      <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div className="modal-body flex-modal-body">
+                      <div className="container initiate-modal p-5">
+                        <div className="">
+                          <h3 className="text-dark fw-bold">Friends</h3>
+                        </div>
+                        <div className="row mt-5 friends">
+                          <div className="col-md-2 text-center">
+                            <Link><img src={search} alt="" className="img-fluid" width={150} /></Link>
+                            <p className="text-center">Search</p>
+                          </div>
+                          <div className="col-md-10">
+                            <div className="owl-carousel owl-friends owl-theme">
+                              <div className="media-29101">
+                                <img src={user} alt="Image" className="img-fluid"/>
+                                <p className="text-center">{"<"}StarBoy{"/>"}</p>
+                              </div>
+                              <div className="media-29101">
+                                <img src={user} alt="Image" className="img-fluid"/>
+                                <p className="text-center">{"<"}StarBoy{"/>"}</p>
+                              </div>
+                              <div className="media-29101">
+                                <img src={user} alt="Image" className="img-fluid"/>
+                                <p className="text-center">{"<"}StarBoy{"/>"}</p>
+                              </div>
+                              <div className="media-29101">
+                                <img src={user} alt="Image" className="img-fluid"/>
+                                <p className="text-center">{"<"}StarBoy{"/>"}</p>
+                              </div>
+                              <div className="media-29101">
+                                <img src={user} alt="Image" className="img-fluid"/>
+                                <p className="text-center">{"<"}StarBoy{"/>"}</p>
+                              </div>
+                              <div className="media-29101">
+                                <img src={user} alt="Image" className="img-fluid"/>
+                                <p className="text-center">{"<"}StarBoy{"/>"}</p>
+                              </div>
+                              <div className="media-29101">
+                                <img src={user} alt="Image" className="img-fluid"/>
+                                <p className="text-center">{"<"}StarBoy{"/>"}</p>
+                              </div>
+                              <div className="media-29101">
+                                <img src={user} alt="Image" className="img-fluid"/>
+                                <p className="text-center">{"<"}StarBoy{"/>"}</p>
+                              </div>
+                              
+                            </div>
+                          </div>
+                        </div>
+                        <div className="row mt-4">
+                          <div className="search-box">
+                            <input type="text" class="search-input" placeholder="Search"/>
+                            <button className="search-button">
+                              <i className="fas fa-search"></i>
+                            </button>
+                          </div>
+                        </div>
+                        <div className="row mt-4">
+                          <div className="col friends-list">
+                            <Link data-bs-toggle="modal" data-bs-target="#user-send-money" type="button" className="d-flex flex-row border-bottom py-3">
+                              <img src={becca} alt="" className="img-fluid" />
+                              <p className="ms-2 mt-3">{"<"}StarBoy{"/>"}</p>
+                            </Link>
+                            <Link data-bs-toggle="modal" data-bs-target="#user-send-money" type="button" className="d-flex flex-row border-bottom py-3">
+                              <img src={becca} alt="" className="img-fluid" />
+                              <p className="ms-2 mt-3">{"<"}StarBoy{"/>"}</p>
+                            </Link>
+                            <Link data-bs-toggle="modal" data-bs-target="#user-send-money" type="button" className="d-flex flex-row border-bottom py-3">
+                              <img src={becca} alt="" className="img-fluid" />
+                              <p className="ms-2 mt-3">{"<"}StarBoy{"/>"}</p>
+                            </Link>
+                            <Link data-bs-toggle="modal" data-bs-target="#user-send-money" type="button" className="d-flex flex-row border-bottom py-3">
+                              <img src={becca} alt="" className="img-fluid" />
+                              <p className="ms-2 mt-3">{"<"}StarBoy{"/>"}</p>
+                            </Link>
+                            
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* Send Money to Saving Plan */}
+              <div className="modal flex-modal fade" id="dilla-send-saving-plan" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog right-dialog">
+                  <div className="modal-content right-content">
+                    <div className="modal-header">
+                      <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div className="modal-body flex-modal-body">
+                      <div className="container initiate-modal p-5">
+                        <div className="row mx-3 mt-4 title-card">
+                          <div className="col">
+                            <h3>Savings Plan</h3>
+                          </div>
+                        </div>
+                        <div className="row mx-3 mysan mt-4">
+                          <Link className="border-bottom pb-3 mb-3" data-bs-toggle="modal" data-bs-target="#dilla-send-target" type="button">
+                            <div className="d-flex flex-row">
+                              <img src={mysan} alt="" className="img-fluid" />
+                              <div className="ms-2 mt-2">
+                                <h2>Target</h2>
+                              </div>
+                            </div>
+                            <i className="bi bi-chevron-right float-end" style={{marginTop: "-29px"}}></i>
+                          </Link>
+                          <Link className="border-bottom pb-3 mb-3" data-bs-toggle="modal" data-bs-target="" type="button">
+                            <div className="d-flex flex-row">
+                              <img src={dillauser} alt="" className="img-fluid" />
+                              <div className="ms-2 mt-2">
+                                <h2>Flex</h2>
+                              </div>
+                            </div>
+                            <i className="bi bi-chevron-right float-end" style={{marginTop: "-29px"}}></i>
+                          </Link>
+                          <Link className="border-bottom pb-3 mb-3" data-bs-toggle="modal" data-bs-target="" type="button">
+                            <div className="d-flex flex-row">
+                              <img src={savingsplan} alt="" className="img-fluid" />
+                              <div className="ms-2 mt-2">
+                                <h2>Safe Lock</h2>
+                              </div>
+                            </div>
+                            <i className="bi bi-chevron-right float-end" style={{marginTop: "-29px"}}></i>
+                          </Link>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* Send Money to Saving Plan -Target */}
+              <div className="modal flex-modal fade" id="dilla-send-target" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog right-dialog">
+                  <div className="modal-content right-content">
+                    <div className="modal-header">
+                      <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div className="modal-body flex-modal-body">
+                      <div className="container initiate-modal p-5">
+                        <div className="row justify-content-center">
+                          <div className="col-md-6 text-center ">
+                            <img src={cadet} alt="" className="img-fluid" />
+                            <h2 className="mt-3">{"<"}StarBoy{"/>"}</h2>
+                          </div>
+                        </div>
+                        <div className="row mx-3 mt-3">
+                          <div className="col">
+                            <div className="row t-card p-4 bg-white">
+                              <div className="col-md-6">
+                                <img src={dillalogo} alt="" className="img-fluid" />
+                              </div>
+                              <div className="col-md-6 text-end">
+                                <h4>₦450,000.00</h4>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="row">
+                          <div className="col text-center">
+                            <i className="bi bi-arrow-down fs-4" style={{color: "#3C0071"}}></i>
+                          </div>
+                        </div>
+                        <div className="row mx-3 mt-3">
+                          <div className="col">
+                            <div className="row t-card p-4 bg-white">
+                              <div className="col-md-6">
+                                <h2>Target</h2>
+                                {/* <img src={sanlogo} alt="" className="img-fluid" /> */}
+                              </div>
+                              <div className="col-md-6 text-end">
+                                <h4>₦50,000.00</h4>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="row mx-3">
+                          <div className="mt-3">
+                            <h3>Pin</h3>
+                            <form>
+                              <div className="mb-3 mt-1">
+                                <input type="number" className="form-control target-form" placeholder="Enter Pin" required/>
+                              </div>
+                            </form>
+                          </div>
+                        </div>
+                        
+                        <div className="row mx-3 btn-bottom">
+                          <Link data-bs-toggle="modal" data-bs-target="#dillatotarget" type="button" className="btn btn-outline-primary px-5 py-3 ardilla-btn fs-6 mt-5" to="" style={{width: "100%"}}>Continue</Link>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* Send Money to Saving Plan - Target - Success */}
+              <div className="modal flex-modal fade" id="dillatotarget" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog right-dialog">
+                  <div className="modal-content right-content">
+                    <div className="modal-header">
+                      <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div className="modal-body flex-modal-body">
+                      <div className="container initiate-modal p-5">
+                        <div className="row justify-content-center">
+                          <div className="col-md-6 text-center ">
+                            <img src={successful} alt="" className="img-fluid" />
+                            <h2 className="mt-4 mb-2">{"<"}StarBoy{"/>"}</h2>
+                            <p className="mb-3">0708 7788 7890</p>
+                          </div>
+                        </div>
+                        <div className="row my-3 sent-details">
+                          <div className="col text-center">
+                            <img src={sent} alt="" className="img-fluid" />
+                            <h3 className="my-3">Money Transfered</h3>
+                            <p>You have successfully sent <span style={{color: "#3D0072"}}>NGN50000 to your <br/>Target Savings Plan</span></p>
                           </div>
                         </div>
                       </div>
