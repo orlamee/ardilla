5b442411dd1086d6227c6626549e10fc5b3713b7
orlamee <meetmichaelakindele@gmail.com>
2023-01-04 11:38:26 +0100

    login page

--- src/components/login.js
+++ src/components/login.js
@@ -1,6 +1,5 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState, useEffect } from "react";
-import home from "../img/home-login.svg";
 import logo from "../img/logo.svg";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
@@ -139,7 +138,7 @@ function Login() {
   };
 
   return (
-    <section className="login-section">
+    <section className="">
       {err && (
         <div className="row justify-content-center  ardilla-alert">
           <div className="col-md-6">
@@ -180,7 +179,7 @@ function Login() {
           </div>
         </div>
       )}
-      <div className="container">
+      {/* <div className="container">
         <div className="row logo">
           <div className="col-md-6 ">
             <Link to="/">
@@ -188,131 +187,123 @@ function Login() {
             </Link>
           </div>
         </div>
-        <div className="row justify-content-center">
-          <div className="col-md-6 mb-3">
-            <div className="left text-center">
-              <img src={home} alt="" className="img-fluid" />
-              <h2 className="my-4">The future of savings & Investment</h2>
-              <h6 className="mb-4">
-                <span className="qoute">"</span>Saving money typically means it
-                is available when we need it
-                <br />
-                and it has a low risk of losing value.
-                <span className="qoute">"</span>
-              </h6>
 
-              <div className="d-flex justify-content-center mb-3">
-                <div className="mt-3 me-2">
-                  <p className="text-center users">New user?</p>
-                </div>
-                <div>
-                  <Link
-                    className="btn btn-outline-primary px-5 py-3 ardilla-btn"
-                    to="/sign-up"
-                  >
-                    Create account
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-5">
-            <div className="right">
-              <div className="login-form">
-                <h3>Welcome!</h3>
-                <h6 className="mt-4">
-                  Here’s how to Log in to access your account
-                </h6>
-                <form className="mt-5" onSubmit={handleSubmit}>
-                  <div className="mb-3">
-                    <label className="form-label">
-                      <i className="bi bi-envelope-fill me-2"></i> Email address
-                    </label>
-                    <input
-                      type="email"
-                      className="form-control custom-login"
-                      required
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value.toLowerCase())}
-                    />
-                  </div>
-                  <div className="">
-                    <label className="form-label">
-                      <i className="bi bi-shield-lock-fill me-2"></i> Password
-                    </label>
-                    <input
-                      type="password"
-                      name="pass"
-                      className="form-control custom-login"
-                      id="pass"
-                      required
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <span
-                      toggle="#password-field"
-                      className="fa fa-fw fa-eye field_icon toggle-pass"
-                    ></span>
-                  </div>
-                  <div className="mobile">
-                    <div className="mb-2">
-                      <label className="form-check-label remember">
-                        <input type="checkbox" className="form-check-input" />{" "}
-                        Remember me
-                      </label>
+      </div> */}
+      <div className="d-lg-flex half">
+        <div className="bg order-1 order-md-2 new-aside"></div>
+          <div className="contents order-2 order-md-1">
+            <div className="container">
+              <div className="row align-items-center justify-content-center">
+                <div className="col-md-7">
+                  
+                  <div className="right ms-3">
+                    <div className="row logo-new desktop">
+                      <div className="col-md-6 ">
+                        <Link to="/">
+                          <img src={logo} alt="" className="img-fluid mb-5" />
+                        </Link>
+                      </div>
                     </div>
-                    <div>
-                      <Link className="fg" to="/recover-password">
-                        Forgot Password?
-                      </Link>
-                    </div>
-                  </div>
-                  <div className="form-group desktop">
-                    <label className="form-check-label remember">
-                      <input type="checkbox" className="form-check-input" />{" "}
-                      Remember me
-                    </label>
-                    <Link className="float-end fg" to="/recover-password">
-                      Forgot Password?
-                    </Link>
-                  </div>
-                  {loading ? (
-                    <div className="mt-4 mb-5">
+                    <div className="login-form login-form-new">
+                      <h3>Welcome!</h3>
+                      <h6 className="mt-4">
+                        Here’s how to Log in to access your account
+                      </h6>
+                      <form className="mt-5" onSubmit={handleSubmit}>
+                        <div className="mb-3">
+                          <label className="form-label">
+                            <i className="bi bi-envelope-fill me-2"></i> Email address
+                          </label>
+                          <input
+                            type="email"
+                            className="form-control custom-login"
+                            required
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value.toLowerCase())}
+                          />
+                        </div>
+                        <div className="">
+                          <label className="form-label">
+                            <i className="bi bi-shield-lock-fill me-2"></i> Password
+                          </label>
+                          <input
+                            type="password"
+                            name="pass"
+                            className="form-control custom-login"
+                            id="pass"
+                            required
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                          />
+                          <span
+                            toggle="#password-field"
+                            className="fa fa-fw fa-eye field_icon toggle-pass"
+                          ></span>
+                        </div>
+                        <div className="mobile">
+                          <div className="mb-2">
+                            <label className="form-check-label remember">
+                              <input type="checkbox" className="form-check-input" />{" "}
+                              Remember me
+                            </label>
+                          </div>
+                          <div>
+                            <Link className="fg" to="/recover-password">
+                              Forgot Password?
+                            </Link>
+                          </div>
+                        </div>
+                        <div className="form-group desktop">
+                          <label className="form-check-label remember">
+                            <input type="checkbox" className="form-check-input" />{" "}
+                            Remember me
+                          </label>
+                          <Link className="float-end fg" to="/recover-password">
+                            Forgot Password?
+                          </Link>
+                        </div>
+                        {loading ? (
+                          <div className="mt-4 mb-5">
+                            <button
+                              type="button"
+                              className="btn btn-outline-primary px-5 py-3 ardilla-btn"
+                              style={{ width: "100%" }}
+                            >
+                              Loading
+                            </button>
+                          </div>
+                        ) : (
+                          <div className="mt-4 mb-5">
+                            <button
+                              type="submit"
+                              className="btn btn-outline-primary px-5 py-3 ardilla-btn"
+                              style={{ width: "100%" }}
+                            >
+                              Log In
+                            </button>
+                          </div>
+                        )}
+                      </form>
+                      <div className="separator">
+                        <div className="line"></div>
+                        <h6>or</h6>
+                        <div className="line"></div>
+                      </div>
                       <button
                         type="button"
-                        className="btn btn-outline-primary px-5 py-3 ardilla-btn"
-                        style={{ width: "100%" }}
+                        className="mt-5 btn btn-outline-primary py-3 ardilla-btn-outline"
                       >
-                        Loading
+                        <i className="bi bi-box-arrow-in-right me-3"></i> Sign in with
+                        SAN ID
                       </button>
+                      <div className="mt-3 new-sign">
+                        <h5>New user? <Link to="/sign-up" style={{color: "#8807F7"}}>Create account</Link></h5>
+                      </div>
                     </div>
-                  ) : (
-                    <div className="mt-4 mb-5">
-                      <button
-                        type="submit"
-                        className="btn btn-outline-primary px-5 py-3 ardilla-btn"
-                        style={{ width: "100%" }}
-                      >
-                        Log In
-                      </button>
-                    </div>
-                  )}
-                </form>
-                <div className="separator">
-                  <div className="line"></div>
-                  <h6>or</h6>
-                  <div className="line"></div>
+                  </div>
                 </div>
-                <button
-                  type="button"
-                  className="mt-5 btn btn-outline-primary py-3 ardilla-btn-outline"
-                >
-                  <i className="bi bi-box-arrow-in-right me-3"></i> Sign in with
-                  SAN ID
-                </button>
               </div>
             </div>
-          </div>
         </div>
       </div>
     </section>
