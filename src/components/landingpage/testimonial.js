2950b15ed95717b36ffd6c6b32f6cdad63c4e26d
orlamee <meetmichaelakindele@gmail.com>
2023-01-06 14:53:14 +0100

    fixes

--- src/components/landingpage/testimonial.js
+++ src/components/landingpage/testimonial.js
@@ -13,7 +13,7 @@ function Testimonial() {
           <div className="col-md-5 right-contact">
             <h3>5 Million + customers</h3>
             <p>Join millions of Ardilla users building wealth everyday</p>
-            <h3 className="top-products">5 Million + customers</h3>
+            <h3 className="top-products">24/7 Customer support</h3>
             <p>Our customer support team is always available for your requests and complaints. You can contact us through the app, sliding into social media DM's, sending an email to support@ardilla.africa, or calling +0213456789.</p>
           </div>
         </div>
