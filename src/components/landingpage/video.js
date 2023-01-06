97a15ef83b6edb42acf5888c7ebf68b8784248a4
orlamee <meetmichaelakindele@gmail.com>
2023-01-06 12:21:07 +0100

    fixes

--- src/components/landingpage/video.js
+++ src/components/landingpage/video.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import opp from "../../img/opp.svg";
 function Videos() {
   return(
     <section className="videos">
@@ -6,10 +8,23 @@ function Videos() {
         <div className="row">
           <div className="col-md-9">
             <div className="d-inline-flex badge">Up to 30% returns</div>
-            <h2 className="mt-5">Unlimited Investment Opportunities </h2>
+            <h2 className="mt-5"><span style={{color: "#3D0072"}}>Unlimited</span> Investment Opportunities </h2>
           </div>
         </div>
-        
+        <div className="row mt-5">
+          <div className="col-md-6 top-products mb-3">
+            <h5 className="mb-5">Plan towards your future by investing with ardilla,  Grow your money<br/>confidently and securely with the available investment oppurtuinities  </h5>
+            <Link>Learn more about investments <i className="bi bi-chevron-right"></i></Link>
+          </div>
+          <div className="col-md-6">
+            <img src={opp} alt="opportunities" className="img-fluid" />
+          </div>
+        </div>
+        <div className="row top-access">
+          <div className="col-md-9">
+            <h2><span style={{color: "#E8356D"}}>Access More</span><span style={{color: "#3D0072"}}> With Your Money</span></h2>
+          </div>
+        </div>
       </div>
     </section>
   )
