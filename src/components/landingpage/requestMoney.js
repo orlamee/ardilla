b21ba7143ffaa4e3786b6fca6b7e4d56f32fc868
Olamilekan Akindele <olalekan@astract.com.ng>
2023-01-08 22:28:16 +0100

    fixes

--- src/components/landingpage/requestMoney.js
+++ src/components/landingpage/requestMoney.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import mon from "../../img/reqq.png";
+import mon from "../../img/reqq.svg";
 
 
 function RequestMoney() {
@@ -11,7 +11,7 @@ function RequestMoney() {
            <h2 className="text-white">Request Money</h2>
            <h6 className="mt-3 dilla-text text-white">Send money safely and conveniently to friends and families. Send to Dilla wallets or bank accounts.</h6>
           </div>
-          {/* <img src={mon} alt="send money" className="img-fluid mt-5" /> */}
+          <img src={mon} alt="send money" className="img-fluid mt-5" />
         </div>
       </div>
     </section>
