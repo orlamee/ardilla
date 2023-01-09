c4069b7b8587d013d0398fe89524b8c82a9eb5d3
overlord <rexatuzie@gmail.com>
2023-01-09 10:58:55 +0100

    hj,fnkj

--- src/components/dashboardParts/profileKyc.js
+++ src/components/dashboardParts/profileKyc.js
@@ -378,99 +378,91 @@ function ProfileKYC() {
                   </form>
                 </div>
               )}
+
+              {userDetails?.idFrontStatus === "pending" && (
+                <div className="row mt-5">
+                  <div className="col-md-4">
+                    <div className="card-upload text-center p-4 review-bg">
+                      <img src={check} alt="" className="img-fluid" />
+                      <h6 className="mt-2">
+                        Upload Succesful Awaiting confirmation...
+                      </h6>
+                    </div>
+                  </div>
+                </div>
+              )}
+
+              {userDetails?.idBackStatus === "pending" && (
+                <div className="row mt-5">
+                  <div className="col-md-4">
+                    <div className="card-upload text-center p-4 review-bg">
+                      <img src={check} alt="" className="img-fluid" />
+                      <h6 className="mt-2">
+                        Upload Succesful Awaiting confirmation...
+                      </h6>
+                    </div>
+                  </div>
+                </div>
+              )}
+
+              {userDetails?.idFrontStatus === "approve" && (
+                <div className="row mt-5">
+                  <h5>
+                    Upload Your valid ID{" "}
+                    <img src={pass} alt="success" className="img-fluid ms-2" />
+                  </h5>
+                </div>
+              )}
+
+              {userDetails?.idBackStatus === "approve" && (
+                <div className="row mt-5">
+                  <h5>
+                    Upload Your valid ID{" "}
+                    <img src={pass} alt="success" className="img-fluid ms-2" />
+                  </h5>
+                </div>
+              )}
+
+              {userDetails?.idFrontStatus === "decline" && (
+                <div>
+                  <div className="row mt-5">
+                    <h5>
+                      Upload Your valid ID{" "}
+                      <small style={{ color: "#E8356D" }}>
+                        Document not auntheticated, please reupload
+                      </small>
+                    </h5>
+                  </div>
+                  <div className="col-md-4">
+                    <div className="card-upload text-center p-4">
+                      <img src={up} alt="" className="img-fluid" />
+                      <h6>Upload the Front View</h6>
+                      {/* <h5>Drag and drop image, or <span className="fw-bold" style={{color: "#E8356D"}}>Browse</span></h5> */}
+                    </div>
+                  </div>
+                </div>
+              )}
+
+              {userDetails?.idBackStatus === "decline" && (
+                <div>
+                  <div className="row mt-5">
+                    <h5>
+                      Upload Your valid ID{" "}
+                      <small style={{ color: "#E8356D" }}>
+                        Document not auntheticated, please reupload
+                      </small>
+                    </h5>
+                  </div>
+                  <div className="col-md-4">
+                    <div className="card-upload text-center p-4">
+                      <img src={up} alt="" className="img-fluid" />
+                      <h6>Upload the Front View</h6>
+                      {/* <h5>Drag and drop image, or <span className="fw-bold" style={{color: "#E8356D"}}>Browse</span></h5> */}
+                    </div>
+                  </div>
+                </div>
+              )}
             </div>
-
-            {userDetails?.idFrontStatus === "pending" && (
-              // <div>
-              // <div className="row mt-5">
-              //   <h5>Upload Your valid ID</h5>
-              // </div>
-              <div className="row mt-5">
-                <div className="col-md-4">
-                  <div className="card-upload text-center p-4 review-bg">
-                    <img src={check} alt="" className="img-fluid" />
-                    <h6 className="mt-2">
-                      Upload Succesful Awaiting confirmation...
-                    </h6>
-                  </div>
-                </div>
-              </div>
-            )}
-
-            {userDetails?.idBackStatus === "pending" && (
-              // <div>
-              // <div className="row mt-5">
-              //   <h5>Upload Your valid ID</h5>
-              // </div>
-              <div className="row mt-5">
-                <div className="col-md-4">
-                  <div className="card-upload text-center p-4 review-bg">
-                    <img src={check} alt="" className="img-fluid" />
-                    <h6 className="mt-2">
-                      Upload Succesful Awaiting confirmation...
-                    </h6>
-                  </div>
-                </div>
-              </div>
-            )}
-
-            {userDetails?.idFrontStatus === "approve" && (
-              <div className="row mt-5">
-                <h5>
-                  Upload Your valid ID{" "}
-                  <img src={pass} alt="success" className="img-fluid ms-2" />
-                </h5>
-              </div>
-            )}
-
-            {userDetails?.idBackStatus === "approve" && (
-              <div className="row mt-5">
-                <h5>
-                  Upload Your valid ID{" "}
-                  <img src={pass} alt="success" className="img-fluid ms-2" />
-                </h5>
-              </div>
-            )}
-
-            {userDetails?.idFrontStatus === "decline" && (
-              <div>
-                <div className="row mt-5">
-                  <h5>
-                    Upload Your valid ID{" "}
-                    <small style={{ color: "#E8356D" }}>
-                      Document not auntheticated, please reupload
-                    </small>
-                  </h5>
-                </div>
-                <div className="col-md-4">
-                  <div className="card-upload text-center p-4">
-                    <img src={up} alt="" className="img-fluid" />
-                    <h6>Upload the Front View</h6>
-                    {/* <h5>Drag and drop image, or <span className="fw-bold" style={{color: "#E8356D"}}>Browse</span></h5> */}
-                  </div>
-                </div>
-              </div>
-            )}
-
-            {userDetails?.idBackStatus === "decline" && (
-              <div>
-                <div className="row mt-5">
-                  <h5>
-                    Upload Your valid ID{" "}
-                    <small style={{ color: "#E8356D" }}>
-                      Document not auntheticated, please reupload
-                    </small>
-                  </h5>
-                </div>
-                <div className="col-md-4">
-                  <div className="card-upload text-center p-4">
-                    <img src={up} alt="" className="img-fluid" />
-                    <h6>Upload the Front View</h6>
-                    {/* <h5>Drag and drop image, or <span className="fw-bold" style={{color: "#E8356D"}}>Browse</span></h5> */}
-                  </div>
-                </div>
-              </div>
-            )}
           </div>
 
           <div className="row mt-5">
