2bf42a2a958c5185c16070b12dbab4a5a7561fd1
overlord <rexatuzie@gmail.com>
2023-01-09 16:16:43 +0100

    hfbrkj,

--- src/components/verifyPhone.js
+++ src/components/verifyPhone.js
@@ -27,7 +27,9 @@ function VerifyPhone() {
   const [wrongContactErr, setWrongContactErr] = useState("");
   const [wrongContactMsg, setWrongContactMsg] = useState("");
   const [wrongContactSuc, setWrongContactSuc] = useState("");
-  const [code, setCode] = useState("");
+  // const [code, setCode] = useState("");
+
+  let code;
 
   useEffect(() => {
     try {
@@ -36,9 +38,11 @@ function VerifyPhone() {
           `https://ardilla.herokuapp.com/ardilla/api/user/find/${user._id}`
         );
 
-        console.log("better");
+        console.log("better man");
 
-        setCode(data.user.mobilePinId);
+        // setCode(data.user.mobilePinId);
+
+        code = data.user.mobilePinId;
 
         if (data?.user?.verified === "sq") {
           return;
@@ -91,7 +95,7 @@ function VerifyPhone() {
 
       const pin = data.pinId;
       // pinRef.current = data.pinId;
-      setCode(data.pinId);
+      // setCode(data.pinId);
 
       //update mobile verif pin
 
