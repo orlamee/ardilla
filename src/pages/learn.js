89f01b2bc6900214cdffe133485a185e4f0ba9dc
orlamee <meetmichaelakindele@gmail.com>
2023-01-09 16:38:17 +0100

    fixes

--- src/pages/learn.js
+++ src/pages/learn.js
@@ -3,16 +3,16 @@ import Footer from "../components/footer";
 import Board from "../components/landingpage/leadership";
 import LearnHero from "../components/landingpage/learnHero";
 import ReadBlog from "../components/landingpage/readBlog";
-// import LearnHeader from "../components/landingpage/navBarLearn";
-import NavbarSave from "../components/landingpage/saveNavbar";
+import LearnHeader from "../components/landingpage/navBarLearn";
+// import NavbarSave from "../components/landingpage/saveNavbar";
 import StartQuiz from "../components/landingpage/startQuiz";
 import WatchVideos from "../components/landingpage/watchVideos";
 
 function Learn() {
   return (
     <div className="bg-white">
-      {/* <LearnHeader/> */}
-      <NavbarSave/>
+      <LearnHeader/>
+      {/* <NavbarSave/> */}
       <LearnHero/>
       <ReadBlog/>
       <WatchVideos/>
