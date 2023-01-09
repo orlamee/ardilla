6058575d161ff73d9bacc22cce1b4d574b603f88
overlord <rexatuzie@gmail.com>
2023-01-09 15:55:23 +0100

    tefvg b

--- src/components/verifyPhone.js
+++ src/components/verifyPhone.js
@@ -55,6 +55,8 @@ function VerifyPhone() {
     }
   }, [user._id, navigate]);
 
+  console.log(code);
+
   const fullpin = `${otp1}${otp2}${otp3}${otp4}${otp5}${otp6}`;
 
   const updateProcess = async () => {
