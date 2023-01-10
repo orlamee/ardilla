24c22f2236c65765ffb1b264b9df8018d9b354ff
orlamee <meetmichaelakindele@gmail.com>
2023-01-10 23:29:28 +0100

    fixes

--- src/index.js
+++ src/index.js
@@ -104,7 +104,7 @@ import PrivacyPolicy from "./pages/privacyPolicy";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter onUpdate={() => window.scrollTo(0, 0)}>
+  <BrowserRouter>
     {/* <ScrollToTop> */}
     <Routes>
       <Route path="/" element={<App />}></Route>
