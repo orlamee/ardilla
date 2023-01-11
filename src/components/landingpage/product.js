3c8a541e41327e1b0a75ff25f3d43e6e53a07a36
orlamee <meetmichaelakindele@gmail.com>
2023-01-11 12:11:53 +0100

    fixes

--- src/components/landingpage/product.js
+++ src/components/landingpage/product.js
@@ -9,37 +9,165 @@ function Products() {
           <h2>Products</h2>
         </div>
         <div className="row top-products">
-          <div className="col-md-6">
-            <h3>Save</h3>
-            <h6>Save at your own pace!</h6>
-            <p>Save your money daily, weekly, or monthly. No pressure. We work at<br/>your pace</p>
-            <div className="top-products">
-              <div className="d-flex flex-row mb-4">
-                <img src={checklist} alt="check" className="img-fluid"/>
-                <h5 className="ms-4 mt-1">Create Unlimted savings plans</h5>
-              </div>
-              <div className="d-flex flex-row mb-4">
-                <img src={checklist} alt="check" className="img-fluid"/>
-                <h5 className="ms-4 mt-1">Withdraw anytime</h5>
-              </div>
-              <div className="d-flex flex-row mb-4">
-                <img src={checklist} alt="check" className="img-fluid"/>
-                <h5 className="ms-4 mt-1">Save with Friends with SAN ID</h5>
-              </div>
-              <div className="d-flex flex-row mb-4">
-                <img src={checklist} alt="check" className="img-fluid"/>
-                <h5 className="ms-4 mt-1">Be better at saving</h5>
-              </div>
-              <div className="d-flex flex-row mb-4">
-                <img src={checklist} alt="check" className="img-fluid"/>
-                <h5 className="ms-4 mt-1">Accountability profile</h5>
-              </div>
-              <div className="d-flex flex-row mb-4">
-                <img src={checklist} alt="check" className="img-fluid"/>
-                <h5 className="ms-4 mt-1">Activity and report</h5>
+          <div className="col-md-6 product-scroller" id="style-8">
+            <div className="scrollbar">
+              <h3>Save</h3>
+              <h6>Save at your own pace!</h6>
+              <p>Save your money daily, weekly, or monthly. No pressure. We work at<br/>your pace</p>
+              <div className="top-products">
+                <div className="d-flex flex-row mb-4">
+                  <img src={checklist} alt="check" className="img-fluid"/>
+                  <h5 className="ms-4 mt-1">Create Unlimted savings plans</h5>
+                </div>
+                <div className="d-flex flex-row mb-4">
+                  <img src={checklist} alt="check" className="img-fluid"/>
+                  <h5 className="ms-4 mt-1">Withdraw anytime</h5>
+                </div>
+                <div className="d-flex flex-row mb-4">
+                  <img src={checklist} alt="check" className="img-fluid"/>
+                  <h5 className="ms-4 mt-1">Save with Friends with SAN ID</h5>
+                </div>
+                <div className="d-flex flex-row mb-4">
+                  <img src={checklist} alt="check" className="img-fluid"/>
+                  <h5 className="ms-4 mt-1">Be better at saving</h5>
+                </div>
+                <div className="d-flex flex-row mb-4">
+                  <img src={checklist} alt="check" className="img-fluid"/>
+                  <h5 className="ms-4 mt-1">Accountability profile</h5>
+                </div>
+                <div className="d-flex flex-row mb-4">
+                  <img src={checklist} alt="check" className="img-fluid"/>
+                  <h5 className="ms-4 mt-1">Activity and report</h5>
+                </div>
               </div>
             </div>
+            <div className="scrollbar mt-5">
+              <h3>Learn</h3>
+              <h6>Financial freedom begins with You and Ardilla</h6>
+              <p>Ardila offers wealth-building tips from great financial minds to help you<br/>get to where you need to be.</p>
+              <div className="top-products">
+                <div className="d-flex flex-row mb-4">
+                  <img src={checklist} alt="check" className="img-fluid"/>
+                  <h5 className="ms-4 mt-1">Learn to to invest in 2hrs</h5>
+                </div>
+                <div className="d-flex flex-row mb-4">
+                  <img src={checklist} alt="check" className="img-fluid"/>
+                  <h5 className="ms-4 mt-1">Get a portfolio manager</h5>
+                </div>
+                <div className="d-flex flex-row mb-4">
+                  <img src={checklist} alt="check" className="img-fluid"/>
+                  <h5 className="ms-4 mt-1">Stay on top of your finances</h5>
+                </div>
+                <div className="d-flex flex-row mb-4">
+                  <img src={checklist} alt="check" className="img-fluid"/>
+                  <h5 className="ms-4 mt-1">Learn with family and friends</h5>
+                </div>
+                <div className="d-flex flex-row mb-4">
+                  <img src={checklist} alt="check" className="img-fluid"/>
+                  <h5 className="ms-4 mt-1">Earn points and reward</h5>
+                </div>
+                <div className="d-flex flex-row mb-4">
+                  <img src={checklist} alt="check" className="img-fluid"/>
+                  <h5 className="ms-4 mt-1">Referral benefits</h5>
+                </div>
+              </div>
+            </div>
+            <div className="scrollbar mt-5">
+              <h3>Invest</h3>
+              <h6>Financial freedom begins with You and Ardilla</h6>
+              <p>Ardila offers wealth-building tips from great financial minds to help you<br/>get to where you need to be.</p>
+              <div className="top-products">
+                <div className="d-flex flex-row mb-4">
+                  <img src={checklist} alt="check" className="img-fluid"/>
+                  <h5 className="ms-4 mt-1">Learn to to invest in 2hrs</h5>
+                </div>
+                <div className="d-flex flex-row mb-4">
+                  <img src={checklist} alt="check" className="img-fluid"/>
+                  <h5 className="ms-4 mt-1">Get a portfolio manager</h5>
+                </div>
+                <div className="d-flex flex-row mb-4">
+                  <img src={checklist} alt="check" className="img-fluid"/>
+                  <h5 className="ms-4 mt-1">Stay on top of your finances</h5>
+                </div>
+                <div className="d-flex flex-row mb-4">
+                  <img src={checklist} alt="check" className="img-fluid"/>
+                  <h5 className="ms-4 mt-1">Learn with family and friends</h5>
+                </div>
+                <div className="d-flex flex-row mb-4">
+                  <img src={checklist} alt="check" className="img-fluid"/>
+                  <h5 className="ms-4 mt-1">Earn points and reward</h5>
+                </div>
+                <div className="d-flex flex-row mb-4">
+                  <img src={checklist} alt="check" className="img-fluid"/>
+                  <h5 className="ms-4 mt-1">Referral benefits</h5>
+                </div>
+              </div>
+            </div>
+            <div className="scrollbar mt-5">
+              <h3>Insure</h3>
+              <h6>Financial freedom begins with You and Ardilla</h6>
+              <p>Ardila offers wealth-building tips from great financial minds to help you<br/>get to where you need to be.</p>
+              <div className="top-products">
+                <div className="d-flex flex-row mb-4">
+                  <img src={checklist} alt="check" className="img-fluid"/>
+                  <h5 className="ms-4 mt-1">Learn to to invest in 2hrs</h5>
+                </div>
+                <div className="d-flex flex-row mb-4">
+                  <img src={checklist} alt="check" className="img-fluid"/>
+                  <h5 className="ms-4 mt-1">Get a portfolio manager</h5>
+                </div>
+                <div className="d-flex flex-row mb-4">
+                  <img src={checklist} alt="check" className="img-fluid"/>
+                  <h5 className="ms-4 mt-1">Stay on top of your finances</h5>
+                </div>
+                <div className="d-flex flex-row mb-4">
+                  <img src={checklist} alt="check" className="img-fluid"/>
+                  <h5 className="ms-4 mt-1">Learn with family and friends</h5>
+                </div>
+                <div className="d-flex flex-row mb-4">
+                  <img src={checklist} alt="check" className="img-fluid"/>
+                  <h5 className="ms-4 mt-1">Earn points and reward</h5>
+                </div>
+                <div className="d-flex flex-row mb-4">
+                  <img src={checklist} alt="check" className="img-fluid"/>
+                  <h5 className="ms-4 mt-1">Referral benefits</h5>
+                </div>
+              </div>
+            </div>
+            <div className="scrollbar mt-5">
+              <h3>Budgettings</h3>
+              <h6>Financial freedom begins with You and Ardilla</h6>
+              <p>Ardila offers wealth-building tips from great financial minds to help you<br/>get to where you need to be.</p>
+              <div className="top-products">
+                <div className="d-flex flex-row mb-4">
+                  <img src={checklist} alt="check" className="img-fluid"/>
+                  <h5 className="ms-4 mt-1">Learn to to invest in 2hrs</h5>
+                </div>
+                <div className="d-flex flex-row mb-4">
+                  <img src={checklist} alt="check" className="img-fluid"/>
+                  <h5 className="ms-4 mt-1">Get a portfolio manager</h5>
+                </div>
+                <div className="d-flex flex-row mb-4">
+                  <img src={checklist} alt="check" className="img-fluid"/>
+                  <h5 className="ms-4 mt-1">Stay on top of your finances</h5>
+                </div>
+                <div className="d-flex flex-row mb-4">
+                  <img src={checklist} alt="check" className="img-fluid"/>
+                  <h5 className="ms-4 mt-1">Learn with family and friends</h5>
+                </div>
+                <div className="d-flex flex-row mb-4">
+                  <img src={checklist} alt="check" className="img-fluid"/>
+                  <h5 className="ms-4 mt-1">Earn points and reward</h5>
+                </div>
+                <div className="d-flex flex-row mb-4">
+                  <img src={checklist} alt="check" className="img-fluid"/>
+                  <h5 className="ms-4 mt-1">Referral benefits</h5>
+                </div>
+              </div>
+            </div>
+            
           </div>
+          <div className="col-md-6"></div>
         </div>
         
       </div>
