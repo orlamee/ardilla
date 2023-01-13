f82e019d334a2d0133fb30fb290e54ce498a9f53
orlamee <meetmichaelakindele@gmail.com>
2023-01-13 16:58:17 +0100

    fixes

--- src/components/landingpage/breadcrumbFaq.js
+++ src/components/landingpage/breadcrumbFaq.js
@@ -5,7 +5,7 @@ function BreadcrumbFaq() {
     <section className="breadcrumbfaq">
       <div className="container">
         <div className="row justify-content-center">
-          <div className="col-md-8 text-center" >
+          <div className="col-md-10 text-center" >
             <h2 className="">Frequently Asked Questions (FAQS)</h2>
             <h6 className="mt-3">Find answers to our most frequently asked questions. Can’t find the answer you're looking for? Email our member<br/>support team <span style={{color: "#8807F7"}}>help@Ardilla.com</span></h6>
           </div>
