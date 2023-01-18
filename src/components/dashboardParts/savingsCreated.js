c76d41c2d56285d56609a4e0695ca56b0b46516e
overlord <rexatuzie@gmail.com>
2023-01-18 09:30:57 +0100

    wed commit 11

--- src/components/dashboardParts/savingsCreated.js
+++ src/components/dashboardParts/savingsCreated.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 import home from "../../img/dashboard/home.svg";
 import portfolio from "../../img/dashboard/portfolio.svg";
 import investment from "../../img/dashboard/growth.svg";
@@ -19,68 +19,74 @@ import clan from "../../img/dashboard/clann.svg";
 import vlt from "../../img/dashboard/vlt.svg";
 import graphy from "../../img/dashboard/dibgraph.svg";
 import sta from "../../img/dashboard/statis.svg";
-import { useIdleTimer } from "react-idle-timer";
-import axios from "axios";
-import Cookies from "js-cookie";
+// import { useIdleTimer } from "react-idle-timer";
+// import axios from "axios";
+// import Cookies from "js-cookie";
 
 function SavingsCreated() {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
+
+  // useNavigate
+
+  // useNavigate
+
+  // onClick={handleLogOut}
 
   // let user = JSON.parse(sessionStorage.getItem("user"));
 
-  const refreshToken = async () => {
-    try {
-      const { data } = await axios.get(
-        `https://ardilla.herokuapp.com/ardilla/api/auth/refresh-token/${Cookies.get(
-          "user"
-        )}`
-      );
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // const refreshToken = async () => {
+  //   try {
+  //     const { data } = await axios.get(
+  //       `https://ardilla.herokuapp.com/ardilla/api/auth/refresh-token/${Cookies.get(
+  //         "user"
+  //       )}`
+  //     );
+  //     console.log(data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
-  useEffect(() => {
-    let interval = setInterval(() => {
-      refreshToken();
-    }, 6000);
+  // useEffect(() => {
+  //   let interval = setInterval(() => {
+  //     refreshToken();
+  //   }, 6000);
 
-    return () => clearInterval(interval);
-  }, []);
+  //   return () => clearInterval(interval);
+  // }, []);
 
-  const handleOnIdle = () => {
-    sessionStorage.clear();
-    Cookies.remove("user");
-    navigate("/login");
-    console.log("last active", getLastActiveTime());
-  };
+  // const handleOnIdle = () => {
+  //   sessionStorage.clear();
+  //   Cookies.remove("user");
+  //   navigate("/login");
+  //   console.log("last active", getLastActiveTime());
+  // };
 
-  const { getLastActiveTime } = useIdleTimer({
-    timeout: 1000 * 60 * 2,
-    onIdle: handleOnIdle,
-    events: [
-      "mousemove",
-      "keydown",
-      "wheel",
-      "DOMMouseScroll",
-      "mousewheel",
-      "mousedown",
-      "touchstart",
-      "touchmove",
-      "MSPointerDown",
-      "MSPointerMove",
-      "visibilitychange",
-    ],
-    debounce: 500,
-  });
+  // const { getLastActiveTime } = useIdleTimer({
+  //   timeout: 1000 * 60 * 2,
+  //   onIdle: handleOnIdle,
+  //   events: [
+  //     "mousemove",
+  //     "keydown",
+  //     "wheel",
+  //     "DOMMouseScroll",
+  //     "mousewheel",
+  //     "mousedown",
+  //     "touchstart",
+  //     "touchmove",
+  //     "MSPointerDown",
+  //     "MSPointerMove",
+  //     "visibilitychange",
+  //   ],
+  //   debounce: 500,
+  // });
 
-  const handleLogOut = () => {
-    Cookies.remove("user");
-    sessionStorage.clear();
-    navigate("/login");
-    window.location.reload();
-  };
+  // const handleLogOut = () => {
+  //   Cookies.remove("user");
+  //   sessionStorage.clear();
+  //   navigate("/login");
+  //   window.location.reload();
+  // };
   return (
     <section className="main-dash">
       <div className="sidebar">
@@ -145,7 +151,7 @@ function SavingsCreated() {
               Chat Support
             </div>
           </Link>
-          <Link onClick={handleLogOut}>
+          <Link>
             <div className="d-flex flex-row">
               <img src={logout} alt="" className="img-fluid me-2 icons" />
               Log Out
@@ -161,9 +167,7 @@ function SavingsCreated() {
                 <span className="me-4 san">Total Funds</span>
               </div>
               <div className="p-2 mt-3">
-                <span className="amount">
-                  NGN 400,000.00
-                </span>
+                <span className="amount">NGN 400,000.00</span>
                 <i className="bi bi-eye-fill float-end"></i>
               </div>
               <div className="d-flex flex-row mt-3">
@@ -171,11 +175,18 @@ function SavingsCreated() {
                   <input type="checkbox" placeholder="USD" />
                   <span className="slider round"></span>
                 </label>
-                <span className="ms-2 mt-2 dollar-rate" style={{color: "#B69FC9"}}>₦740 /$1</span>
+                <span
+                  className="ms-2 mt-2 dollar-rate"
+                  style={{ color: "#B69FC9" }}
+                >
+                  ₦740 /$1
+                </span>
               </div>
             </div>
             <div className="mt-4 saving-learn">
-              <Link>Learn More about savings <i className="bi bi-chevron-right"></i></Link>
+              <Link>
+                Learn More about savings <i className="bi bi-chevron-right"></i>
+              </Link>
             </div>
           </div>
           <div className="col-md-2"></div>
@@ -186,8 +197,15 @@ function SavingsCreated() {
                 <h4 className="float-end">See all</h4>
               </div>
               <div className="row mt-5">
-                <div className="col-md-6" style={{marginTop: "-7px"}}>
-                  <h6><img src={withdraw} alt="withdraw" className="img-fluid me-2" /> Savings Withdrawal</h6>
+                <div className="col-md-6" style={{ marginTop: "-7px" }}>
+                  <h6>
+                    <img
+                      src={withdraw}
+                      alt="withdraw"
+                      className="img-fluid me-2"
+                    />{" "}
+                    Savings Withdrawal
+                  </h6>
                 </div>
                 <div className="col-md-3 text-center">
                   <h6>1 hr ago</h6>
@@ -196,10 +214,17 @@ function SavingsCreated() {
                   <h6>₦4,000.00 </h6>
                 </div>
               </div>
-              <hr/>
+              <hr />
               <div className="row mt-2">
-                <div className="col-md-6" style={{marginTop: "-7px"}}>
-                  <h6><img src={withdraw} alt="withdraw" className="img-fluid me-2" /> Savings Withdrawal</h6>
+                <div className="col-md-6" style={{ marginTop: "-7px" }}>
+                  <h6>
+                    <img
+                      src={withdraw}
+                      alt="withdraw"
+                      className="img-fluid me-2"
+                    />{" "}
+                    Savings Withdrawal rex
+                  </h6>
                 </div>
                 <div className="col-md-3 text-center">
                   <h6>1 hr ago</h6>
@@ -208,10 +233,17 @@ function SavingsCreated() {
                   <h6>₦4,000.00 </h6>
                 </div>
               </div>
-              <hr/>
+              <hr />
               <div className="row mt-2">
-                <div className="col-md-6" style={{marginTop: "-7px"}}>
-                  <h6><img src={withdraw} alt="withdraw" className="img-fluid me-2" /> Savings Withdrawal</h6>
+                <div className="col-md-6" style={{ marginTop: "-7px" }}>
+                  <h6>
+                    <img
+                      src={withdraw}
+                      alt="withdraw"
+                      className="img-fluid me-2"
+                    />{" "}
+                    Savings Withdrawal rex
+                  </h6>
                 </div>
                 <div className="col-md-3 text-center">
                   <h6>1 hr ago</h6>
@@ -220,13 +252,13 @@ function SavingsCreated() {
                   <h6>₦4,000.00 </h6>
                 </div>
               </div>
-              <hr/>
+              <hr />
             </div>
           </div>
         </div>
         <div className="row my-5 title-save">
           <div className="col">
-            <h4>My Savings Plan</h4>
+            <h4>My Savings Plan rex</h4>
           </div>
         </div>
         <div className="row">
@@ -242,8 +274,17 @@ function SavingsCreated() {
                       <h3>DIB</h3>
                       <span className="dibbg">12% p.a</span>
                     </div>
-                    <p><span className="text-muted">Recent Top up:</span> <span>20th December, 2022</span></p>
-                    <Link to="/flex-plan" className="btn btn-outline-primary px-3 py-2 ardilla-btn-dib fs-6 mt-2 me-3" style={{background: "#FBBF24", borderColor: "#FBBF24"}}>View Dib</Link>
+                    <p>
+                      <span className="text-muted">Recent Top up:</span>{" "}
+                      <span>20th December, 2022</span>
+                    </p>
+                    <Link
+                      to="/flex-plan"
+                      className="btn btn-outline-primary px-3 py-2 ardilla-btn-dib fs-6 mt-2 me-3"
+                      style={{ background: "#FBBF24", borderColor: "#FBBF24" }}
+                    >
+                      View Dib
+                    </Link>
                   </div>
                 </div>
                 <div className="col-md-3">
@@ -258,19 +299,30 @@ function SavingsCreated() {
         </div>
         <div className="row mt-4">
           <div className="col-md-6 mb-3">
-            <div className="new-pp" style={{background:"#FDF1F5"}}>
+            <div className="new-pp" style={{ background: "#FDF1F5" }}>
               <div className="row">
                 <div className="col-md-2 text-end">
                   <img src={drims} alt="" className="img-fluid" />
                 </div>
                 <div className="col-md-6">
-                   <div className="dibb">
+                  <div className="dibb">
                     <div className="d-flex flex-row mb-2">
                       <h3>DREAMS</h3>
-                      <span className="dibbg" style={{background: "#F8BFD1"}}>12% p.a</span>
+                      <span className="dibbg" style={{ background: "#F8BFD1" }}>
+                        12% p.a
+                      </span>
                     </div>
-                    <p><span className="text-muted">Recent Top up:</span> <span>20th December, 2022</span></p>
-                    <Link to="/target-plan" className="btn btn-outline-primary px-3 py-2 ardilla-btn-dib fs-6 mt-2 me-3" style={{background: "#E8356D", borderColor: "#E8356D"}}>View Dreams</Link>
+                    <p>
+                      <span className="text-muted">Recent Top up:</span>{" "}
+                      <span>20th December, 2022</span>
+                    </p>
+                    <Link
+                      to="/target-plan"
+                      className="btn btn-outline-primary px-3 py-2 ardilla-btn-dib fs-6 mt-2 me-3"
+                      style={{ background: "#E8356D", borderColor: "#E8356D" }}
+                    >
+                      View Dreams
+                    </Link>
                   </div>
                 </div>
                 <div className="col-md-3">
@@ -280,7 +332,7 @@ function SavingsCreated() {
             </div>
           </div>
           <div className="col-md-6 mb-3">
-            <div className="new-pp" style={{background: "#F0FEFA"}}>
+            <div className="new-pp" style={{ background: "#F0FEFA" }}>
               <div className="row">
                 <div className="col-md-2 text-end">
                   <img src={vlt} alt="" className="img-fluid" />
@@ -289,10 +341,21 @@ function SavingsCreated() {
                   <div className="dibb">
                     <div className="d-flex flex-row mb-2">
                       <h3>VAULT</h3>
-                      <span className="dibbg" style={{background: "#BAFCE8"}}>12% p.a</span>
+                      <span className="dibbg" style={{ background: "#BAFCE8" }}>
+                        12% p.a
+                      </span>
                     </div>
-                    <p><span className="text-muted">Recent Top up:</span> <span>20th December, 2022</span></p>
-                    <Link to="/vault" className="btn btn-outline-primary px-3 py-2 ardilla-btn-dib fs-6 mt-2 me-3" style={{background: "#069669", borderColor: "#069669"}}>View Vault</Link>
+                    <p>
+                      <span className="text-muted">Recent Top up:</span>{" "}
+                      <span>20th December, 2022</span>
+                    </p>
+                    <Link
+                      to="/vault"
+                      className="btn btn-outline-primary px-3 py-2 ardilla-btn-dib fs-6 mt-2 me-3"
+                      style={{ background: "#069669", borderColor: "#069669" }}
+                    >
+                      View Vault
+                    </Link>
                   </div>
                 </div>
                 <div className="col-md-3">
@@ -313,10 +376,21 @@ function SavingsCreated() {
                   <div className="dibb">
                     <div className="d-flex flex-row mb-2">
                       <h3>CLAN</h3>
-                      <span className="dibbg" style={{background: "#F6EBFE"}}>12% p.a</span>
+                      <span className="dibbg" style={{ background: "#F6EBFE" }}>
+                        12% p.a
+                      </span>
                     </div>
-                    <p>The family matters. Save collectively or individually for your <br/>family members.</p>
-                    <button className="btn btn-outline-primary px-3 py-2 ardilla-btn-dib fs-6 mt-2 me-3" style={{background: "#8807F7", borderColor: "#8807F7"}}>Create a Clan</button>
+                    <p>
+                      The family matters. Save collectively or individually for
+                      your <br />
+                      family members.
+                    </p>
+                    <button
+                      className="btn btn-outline-primary px-3 py-2 ardilla-btn-dib fs-6 mt-2 me-3"
+                      style={{ background: "#8807F7", borderColor: "#8807F7" }}
+                    >
+                      Create a Clan
+                    </button>
                   </div>
                 </div>
               </div>
@@ -329,13 +403,24 @@ function SavingsCreated() {
                   <img src={grit} alt="" className="img-fluid" />
                 </div>
                 <div className="col-md-8">
-                   <div className="dibb">
+                  <div className="dibb">
                     <div className="d-flex flex-row mb-2">
                       <h3>GRIT</h3>
-                      <span className="dibbg" style={{background: "#F6EBFE"}}>12% p.a</span>
+                      <span className="dibbg" style={{ background: "#F6EBFE" }}>
+                        12% p.a
+                      </span>
                     </div>
-                    <p>For the big-time savers. Save more and get more<br/>interest.</p>
-                    <button className="btn btn-outline-primary px-3 py-2 ardilla-btn-dib fs-6 mt-2 me-3" style={{background: "#8807F7", borderColor: "#8807F7"}}>Create a Grit</button>
+                    <p>
+                      For the big-time savers. Save more and get more
+                      <br />
+                      interest.
+                    </p>
+                    <button
+                      className="btn btn-outline-primary px-3 py-2 ardilla-btn-dib fs-6 mt-2 me-3"
+                      style={{ background: "#8807F7", borderColor: "#8807F7" }}
+                    >
+                      Create a Grit
+                    </button>
                   </div>
                 </div>
               </div>
@@ -348,5 +433,3 @@ function SavingsCreated() {
 }
 
 export default SavingsCreated;
-
-
