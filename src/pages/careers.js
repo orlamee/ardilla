fcd62e4e249c3a7e519cc1850cfb6153f9964f44
orlamee <meetmichaelakindele@gmail.com>
2023-01-23 20:27:29 +0100

    careers

--- src/pages/careers.js
+++ src/pages/careers.js
@@ -2,6 +2,7 @@ import React from "react";
 import Footer from "../components/footer";
 import CareersBreadcrumb from "../components/landingpage/careersBreadcrumb";
 import CareersHeader from "../components/landingpage/careersHeader";
+import Culture from "../components/landingpage/Culture";
 import OurValue from "../components/landingpage/ourValues";
 
 function Careers() {
@@ -9,6 +10,7 @@ function Careers() {
     <div className="bg-white">
       <CareersHeader/>
       <CareersBreadcrumb/>
+      <Culture/>
       <OurValue/>
       
       
