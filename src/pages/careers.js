097703d7c7357e9b323865b452099f04d95a5949
orlamee <meetmichaelakindele@gmail.com>
2023-01-23 17:44:19 +0100

    fixes

--- src/pages/careers.js
+++ src/pages/careers.js
@@ -1,30 +1,30 @@
-import React from "react";
-import Footer from "../components/footer";
-import Culture from "../components/landingpage/ardillaCulture";
-import CareerFooter from "../components/landingpage/careerFooter";
-import CareersBreadcrumb from "../components/landingpage/careersBreadcrumb";
-import CareersHeader from "../components/landingpage/careersHeader";
+// import React from "react";
+// import Footer from "../components/footer";
+// import Culture from "../components/landingpage/ardillaCulture";
+// import CareerFooter from "../components/landingpage/careerFooter";
+// import CareersBreadcrumb from "../components/landingpage/careersBreadcrumb";
+// import CareersHeader from "../components/landingpage/careersHeader";
 // import Hire from "../components/landingpage/howwehire";
-import Openings from "../components/landingpage/openings";
-import OurValue from "../components/landingpage/ourValues";
-import People from "../components/landingpage/people";
-import Perks from "../components/landingpage/perks";
+// import Openings from "../components/landingpage/openings";
+// import OurValue from "../components/landingpage/ourValues";
+// import People from "../components/landingpage/people";
+// import Perks from "../components/landingpage/perks";
 
-function Careers() {
-  return (
-    <div className="bg-white">
-      <CareersHeader/>
-      <CareersBreadcrumb/>
-      <Culture/>
-      <OurValue/>
-      <People/>
-      <Perks/>
-      <Openings/>
-      {/* <Hire/> */}
-      <CareerFooter/>
-      <Footer/>
-    </div>
-  );
-}
+// function Careers() {
+//   return (
+//     <div className="bg-white">
+//       <CareersHeader/>
+//       <CareersBreadcrumb/>
+//       <Culture/>
+//       <OurValue/>
+//       <People/>
+//       <Perks/>
+//       <Openings/>
+//       <Hire/>
+//       <CareerFooter/>
+//       <Footer/>
+//     </div>
+//   );
+// }
 
-export default Careers;
+// export default Careers;
