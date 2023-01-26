91ff4e7dbc769efaaefd1e081d0ef8098c55338f
overlord <rexatuzie@gmail.com>
2023-01-26 17:13:02 +0100

    commit lc 45

--- src/components/dashboardParts/targetPreference.js
+++ src/components/dashboardParts/targetPreference.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import home from "../../img/dashboard/home.svg";
 import portfolio from "../../img/dashboard/portfolio.svg";
 import investment from "../../img/dashboard/growth.svg";
@@ -12,11 +12,138 @@ import logout from "../../img/dashboard/logout.svg";
 import contact from "../../img/dashboard/pay.svg";
 import chat from "../../img/dashboard/chat.svg";
 import final from "../../img/dashboard/prefer.svg";
-
+import axios from "axios";
 
 function TargetPreference() {
+  const [type, setType] = useState("");
+  const [msg, setMsg] = useState("");
+  const [err, setErr] = useState(false);
+  const [onSuccess, setOnSuccess] = useState(false);
+  const [loading, setLoading] = useState();
+
+  const navigate = useNavigate();
+
+  const handleClickSuccess = () => {
+    setOnSuccess(false);
+  };
+
+  const handlePrivate = async () => {
+    if (type) {
+      setLoading(true);
+
+      try {
+        await axios.put(
+          `${process.env.REACT_APP_BACKEND_URL}/api/target/set-type`,
+          {
+            type,
+          },
+          { withCredentials: true }
+        );
+
+        setLoading(false);
+        navigate("/target-overview");
+      } catch (error) {
+        const message =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
+
+        setLoading(false);
+        setErr(true);
+        setMsg(message);
+      }
+    } else {
+      setLoading(false);
+      setErr(true);
+      setMsg(" Please select a Dream type");
+    }
+  };
+
+  const handlePublic = async () => {
+    if (type) {
+      setLoading(true);
+
+      try {
+        await axios.put(
+          `${process.env.REACT_APP_BACKEND_URL}/api/target/set-type`,
+          {
+            type,
+          },
+          { withCredentials: true }
+        );
+
+        setLoading(false);
+        navigate("/target-public-selection");
+      } catch (error) {
+        const message =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
+
+        setLoading(false);
+        setErr(true);
+        setMsg(message);
+      }
+    } else {
+      setLoading(false);
+      setErr(true);
+      setMsg(" Please select a Dream type");
+    }
+  };
+
+  const handleSubmit = () => {
+    if (type === "public") {
+      handlePublic();
+    } else {
+      handlePrivate();
+    }
+  };
   return (
     <section className="main-dash">
+      {err && (
+        <div className="row justify-content-center  ardilla-alert">
+          <div className="col-md-6">
+            <div
+              className="alert alert-danger alert-dismissible fade show text-center text-danger"
+              role="alert"
+            >
+              <i className="bi bi-exclamation-circle me-3"></i>
+              {msg}
+              <button
+                type="button"
+                className="btn-close"
+                // data-bs-dismiss="alert"
+                onClick={() => setErr(false)}
+                aria-label="Close"
+              ></button>
+            </div>
+          </div>
+        </div>
+      )}
+      {onSuccess && (
+        <div className="row justify-content-center mt-5  ardilla-alert">
+          <div className="col-md-6">
+            <div
+              className="alert alert-success alert-dismissible fade show text-center text-success"
+              role="alert"
+            >
+              <i className="bi bi-patch-check-fill me-3"></i>
+              {msg}
+              <button
+                type="button"
+                className="btn-close"
+                // data-bs-dismiss="alert"
+                onClick={handleClickSuccess}
+                aria-label="Close"
+              ></button>
+            </div>
+          </div>
+        </div>
+      )}
       <div className="sidebar">
         <Link to="/dashboard" className="">
           <div className="d-flex flex-row">
@@ -89,27 +216,73 @@ function TargetPreference() {
       </div>
       <div className="content py-5 px-5 earning-section">
         <div className="row backto">
-          <Link to="/target-prefer"><span><i className="bi bi-chevron-left me-3"></i>Back</span></Link>
+          <Link to="/target-prefer">
+            <span>
+              <i className="bi bi-chevron-left me-3"></i>Back
+            </span>
+          </Link>
         </div>
         <div className="row earning">
           <div className="col-md-12 text-center">
-            <img src={final} alt="" className="img-fluid"/>
+            <img src={final} alt="" className="img-fluid" />
             {/* <h2>Cadet {"<"}Starboy{"/>"},</h2> */}
           </div>
         </div>
         <div className="row justify-content-center earns">
           <div className="col-md-8 text-center">
-            <h3>Make <span style={{color: "#E8356D"}}>Target</span> public or<br/>private</h3>
+            <h3>
+              Make <span style={{ color: "#E8356D" }}>Target</span> public or
+              <br />
+              private
+            </h3>
             <p className="my-5">Lorem ipsum antare soon aire uionbu</p>
             <div className="mb-3">
-              <div className="btn-group me-3" role="group" aria-label="First group">
-                <button type="button" className="btn btn-flex">Public</button>
+              <div
+                className="btn-group me-3"
+                role="group"
+                aria-label="First group"
+              >
+                <button
+                  type="button"
+                  className="btn btn-flex"
+                  onClick={(e) => setType("public")}
+                >
+                  Public
+                </button>
               </div>
-              <div className="btn-group me-3" role="group" aria-label="Second group">
-                <button type="button" className="btn btn-flex">Private</button>
+              <div
+                className="btn-group me-3"
+                role="group"
+                aria-label="Second group"
+              >
+                <button
+                  type="button"
+                  className="btn btn-flex"
+                  onClick={(e) => setType("private")}
+                >
+                  Private
+                </button>
               </div>
             </div>
-            <div><Link className="btn btn-outline-primary px-5 py-3 ardilla-btn fs-6 mt-5" to="/target-spend" style={{width: "50%"}}>Continue</Link></div>
+            <div>
+              {loading ? (
+                <Link
+                  className="btn btn-outline-primary px-5 py-3 ardilla-btn fs-6 mt-5"
+                  style={{ width: "50%" }}
+                >
+                  Loading
+                </Link>
+              ) : (
+                <Link
+                  className="btn btn-outline-primary px-5 py-3 ardilla-btn fs-6 mt-5"
+                  // to="/flex-overview"
+                  onClick={handleSubmit}
+                  style={{ width: "50%" }}
+                >
+                  Continue
+                </Link>
+              )}
+            </div>
           </div>
         </div>
       </div>
@@ -118,4 +291,3 @@ function TargetPreference() {
 }
 
 export default TargetPreference;
- 
