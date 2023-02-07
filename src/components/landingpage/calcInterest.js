c2a58240a256cc2aaccb0446ba76f914ec0af289
overlord <rexatuzie@gmail.com>
2023-02-07 13:36:26 +0100

    tue commit 23

--- src/components/landingpage/calcInterest.js
+++ src/components/landingpage/calcInterest.js
@@ -116,9 +116,7 @@ function CalcInterest() {
                   className="form-select int-select"
                   onChange={handleChange}
                 >
-                  <option selected value="daily">
-                    Daily
-                  </option>
+                  <option value="daily">Daily</option>
                   <option value="weekly">Weekly</option>
                   <option value="monthly">Monthly</option>
                   <option value="yearly">Yearly</option>
