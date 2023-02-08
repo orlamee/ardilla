72ad45e46f679cb010cc70a356e4de55a08ef20b
overlord <rexatuzie@gmail.com>
2023-02-08 13:24:29 +0100

    wed commit 1

--- src/components/dashboardParts/targetPlanPublic.js
+++ src/components/dashboardParts/targetPlanPublic.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import home from "../../img/dashboard/home.svg";
 import portfolio from "../../img/dashboard/portfolio.svg";
 import investment from "../../img/dashboard/growth.svg";
@@ -12,11 +12,99 @@ import logout from "../../img/dashboard/logout.svg";
 import contact from "../../img/dashboard/pay.svg";
 import chat from "../../img/dashboard/chat.svg";
 import final from "../../img/dashboard/prefer.svg";
-
+import axios from "axios";
 
 function TargetPlanPublic() {
+  const [type, setType] = useState("");
+  const [msg, setMsg] = useState("");
+  const [err, setErr] = useState(false);
+  const [onSuccess, setOnSuccess] = useState(false);
+  const [loading, setLoading] = useState();
+
+  const navigate = useNavigate();
+
+  const { id } = useParams();
+
+  const handleClickSuccess = () => {
+    setOnSuccess(false);
+  };
+
+  const handleSubmit = async () => {
+    if (type) {
+      setLoading(true);
+
+      try {
+        await axios.put(
+          `${process.env.REACT_APP_BACKEND_URL}/api/target/display-type/${id}`,
+          {
+            type,
+          },
+          { withCredentials: true }
+        );
+
+        setLoading(false);
+        navigate(`/target-public-desc/${id}`);
+      } catch (error) {
+        const message =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
+
+        setLoading(false);
+        setErr(true);
+        setMsg(message);
+      }
+    } else {
+      setLoading(false);
+      setErr(true);
+      setMsg(" Please select a Dream type");
+    }
+  };
+
   return (
     <section className="main-dash">
+      {err && (
+        <div className="row justify-content-center  ardilla-alert">
+          <div className="col-md-6">
+            <div
+              className="alert alert-danger alert-dismissible fade show text-center text-danger"
+              role="alert"
+            >
+              <i className="bi bi-exclamation-circle me-3"></i>
+              {msg}
+              <button
+                type="button"
+                className="btn-close"
+                // data-bs-dismiss="alert"
+                onClick={() => setErr(false)}
+                aria-label="Close"
+              ></button>
+            </div>
+          </div>
+        </div>
+      )}
+      {onSuccess && (
+        <div className="row justify-content-center mt-5  ardilla-alert">
+          <div className="col-md-6">
+            <div
+              className="alert alert-success alert-dismissible fade show text-center text-success"
+              role="alert"
+            >
+              <i className="bi bi-patch-check-fill me-3"></i>
+              {msg}
+              <button
+                type="button"
+                className="btn-close"
+                // data-bs-dismiss="alert"
+                onClick={handleClickSuccess}
+                aria-label="Close"
+              ></button>
+            </div>
+          </div>
+        </div>
+      )}
       <div className="sidebar">
         <Link to="/dashboard" className="">
           <div className="d-flex flex-row">
@@ -89,11 +177,15 @@ function TargetPlanPublic() {
       </div>
       <div className="content py-5 px-5 earning-section">
         <div className="row backto">
-          <Link to="/target-prefer"><span><i className="bi bi-chevron-left me-3"></i>Back</span></Link>
+          <Link to="/target-prefer">
+            <span>
+              <i className="bi bi-chevron-left me-3"></i>Back
+            </span>
+          </Link>
         </div>
         <div className="row earning">
           <div className="col-md-12 text-center">
-            <img src={final} alt="" className="img-fluid"/>
+            <img src={final} alt="" className="img-fluid" />
             {/* <h2>Cadet {"<"}Starboy{"/>"},</h2> */}
           </div>
         </div>
@@ -102,16 +194,55 @@ function TargetPlanPublic() {
             <h3>Public Selection</h3>
             <p className="my-5">Lorem ipsum antare soon aire uionbu</p>
             <div className="mb-3">
-              <div className="btn-group me-3" role="group" aria-label="First group">
-                <button type="button" className="btn btn-flex">Friends</button>
+              <div
+                className="btn-group me-3"
+                role="group"
+                aria-label="First group"
+              >
+                <button
+                  type="button"
+                  value={"friends"}
+                  className="btn btn-flex"
+                  onClick={(e) => setType(e.target.value)}
+                >
+                  Friends
+                </button>
               </div>
-              <div className="btn-group me-3" role="group" aria-label="Second group">
-                <button type="button" className="btn btn-flex">Global</button>
+              <div
+                className="btn-group me-3"
+                role="group"
+                aria-label="Second group"
+              >
+                <button
+                  type="button"
+                  value={"global"}
+                  className="btn btn-flex"
+                  onClick={(e) => setType(e.target.value)}
+                >
+                  Global
+                </button>
               </div>
             </div>
-            <div><Link className="btn btn-outline-primary px-5 py-3 ardilla-btn fs-6 mt-5" to="" style={{width: "50%"}}>Continue</Link></div>
+            <div>
+              {loading ? (
+                <Link
+                  className="btn btn-outline-primary px-5 py-3 ardilla-btn fs-6 mt-5"
+                  style={{ width: "50%" }}
+                >
+                  Loading
+                </Link>
+              ) : (
+                <Link
+                  className="btn btn-outline-primary px-5 py-3 ardilla-btn fs-6 mt-5"
+                  // to="/flex-overview"
+                  onClick={handleSubmit}
+                  style={{ width: "50%" }}
+                >
+                  Continue
+                </Link>
+              )}
+            </div>
           </div>
-          
         </div>
       </div>
     </section>
@@ -119,4 +250,3 @@ function TargetPlanPublic() {
 }
 
 export default TargetPlanPublic;
- 
--- src/components/dashboardParts/targetPublicDesc.js
+++ src/components/dashboardParts/targetPublicDesc.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import home from "../../img/dashboard/home.svg";
 import portfolio from "../../img/dashboard/portfolio.svg";
 import investment from "../../img/dashboard/growth.svg";
@@ -12,11 +12,93 @@ import logout from "../../img/dashboard/logout.svg";
 import contact from "../../img/dashboard/pay.svg";
 import chat from "../../img/dashboard/chat.svg";
 import final from "../../img/dashboard/prefer.svg";
-
+import axios from "axios";
 
 function TargetPublicDesc() {
+  const [description, setDescription] = useState("");
+  const [msg, setMsg] = useState("");
+  const [err, setErr] = useState(false);
+  const [onSuccess, setOnSuccess] = useState(false);
+  const [loading, setLoading] = useState();
+
+  const navigate = useNavigate();
+
+  const { id } = useParams();
+
+  const handleClickSuccess = () => {
+    setOnSuccess(false);
+  };
+
+  const handleSubmit = async () => {
+    setLoading(true);
+
+    try {
+      await axios.put(
+        `${process.env.REACT_APP_BACKEND_URL}/api/target/desc/${id}`,
+        {
+          description,
+        },
+        { withCredentials: true }
+      );
+
+      setLoading(false);
+      navigate(`/target-share-link/${id}`);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+
+      setLoading(false);
+      setErr(true);
+      setMsg(message);
+    }
+  };
+
   return (
     <section className="main-dash">
+      {err && (
+        <div className="row justify-content-center  ardilla-alert">
+          <div className="col-md-6">
+            <div
+              className="alert alert-danger alert-dismissible fade show text-center text-danger"
+              role="alert"
+            >
+              <i className="bi bi-exclamation-circle me-3"></i>
+              {msg}
+              <button
+                type="button"
+                className="btn-close"
+                // data-bs-dismiss="alert"
+                onClick={() => setErr(false)}
+                aria-label="Close"
+              ></button>
+            </div>
+          </div>
+        </div>
+      )}
+      {onSuccess && (
+        <div className="row justify-content-center mt-5  ardilla-alert">
+          <div className="col-md-6">
+            <div
+              className="alert alert-success alert-dismissible fade show text-center text-success"
+              role="alert"
+            >
+              <i className="bi bi-patch-check-fill me-3"></i>
+              {msg}
+              <button
+                type="button"
+                className="btn-close"
+                // data-bs-dismiss="alert"
+                onClick={handleClickSuccess}
+                aria-label="Close"
+              ></button>
+            </div>
+          </div>
+        </div>
+      )}
       <div className="sidebar">
         <Link to="/dashboard" className="">
           <div className="d-flex flex-row">
@@ -89,26 +171,58 @@ function TargetPublicDesc() {
       </div>
       <div className="content py-5 px-5 earning-section">
         <div className="row backto">
-          <Link to="/target-prefer"><span><i className="bi bi-chevron-left me-3"></i>Back</span></Link>
+          <Link to="/target-prefer">
+            <span>
+              <i className="bi bi-chevron-left me-3"></i>Back
+            </span>
+          </Link>
         </div>
         <div className="row earning">
           <div className="col-md-12 text-center">
-            <img src={final} alt="" className="img-fluid"/>
+            <img src={final} alt="" className="img-fluid" />
             {/* <h2>Cadet {"<"}Starboy{"/>"},</h2> */}
           </div>
         </div>
         <div className="row justify-content-center earns">
           <div className="col-md-8 text-center">
-            <h3>Lorem Ipsum Monterem<br/><span style={{color: "#E8356D"}}>target public?</span></h3>
+            <h3>
+              Lorem Ipsum Monterem
+              <br />
+              <span style={{ color: "#E8356D" }}>target public?</span>
+            </h3>
             <div className="row justify-content-center mt-4">
               <div className="col-md-6">
                 <form>
                   <div className="mb-3">
-                    <textarea type="textarea" className="form-control target-form" placeholder="Enter Description" required rows="5"></textarea>
+                    <textarea
+                      onChange={(e) => setDescription(e.target.value)}
+                      type="textarea"
+                      className="form-control target-form"
+                      placeholder="Enter Description"
+                      rows="5"
+                    ></textarea>
                   </div>
-                  <div className="mb-3">
-                    <Link className="btn btn-outline-primary px-5 py-3 ardilla-btn fs-6" to="" style={{width: "100%"}}>Continue</Link>
-                  </div>
+
+                  {loading ? (
+                    <div className="mb-3">
+                      <button
+                        className="btn btn-outline-primary px-5 py-3 ardilla-btn fs-6"
+                        style={{ width: "100%" }}
+                      >
+                        Loading
+                      </button>
+                    </div>
+                  ) : (
+                    <div className="mb-3">
+                      <button
+                        className="btn btn-outline-primary px-5 py-3 ardilla-btn fs-6"
+                        style={{ width: "100%" }}
+                        onClick={handleSubmit}
+                      >
+                        Continue
+                      </button>
+                    </div>
+                  )}
                 </form>
               </div>
             </div>
@@ -120,4 +234,3 @@ function TargetPublicDesc() {
 }
 
 export default TargetPublicDesc;
- 
