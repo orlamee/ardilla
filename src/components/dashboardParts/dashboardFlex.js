116169281719d998b67d6bdae7a5d5bcadb7d027
overlord <rexatuzie@gmail.com>
2023-02-09 12:58:59 +0100

    thu commit 19

--- src/components/dashboardParts/dashboardFlex.js
+++ src/components/dashboardParts/dashboardFlex.js
@@ -527,8 +527,8 @@ function DashboardFlex() {
                   <div className="col-md-6 text-end">
                     <h5 className="text-white">
                       {flexAcct?.paymentDate}-
-                      {/* {flexAcct?.breakdown[0]?.date?.month}-
-                      {flexAcct?.breakdown[0]?.date?.year} */}
+                      {flexAcct?.breakdown[0]?.date?.month}-
+                      {flexAcct?.breakdown[0]?.date?.year}
                     </h5>
                   </div>
                 </div>
@@ -540,8 +540,15 @@ function DashboardFlex() {
                     <h5 className="text-white">
                       {" "}
                       {flexAcct?.paymentDate}-
-                      {/* {flexAcct?.breakdown[1]?.date?.month}-
-                      {flexAcct?.breakdown[1]?.date?.year} */}
+                      {
+                        flexAcct?.breakdown?.[flexAcct?.breakdown?.length - 1]
+                          .date?.month
+                      }
+                      -{" "}
+                      {
+                        flexAcct?.breakdown?.[flexAcct?.breakdown?.length - 1]
+                          .date?.year
+                      }
                     </h5>
                   </div>
                 </div>
