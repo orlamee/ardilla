9c522a5f60aa5f58267319adce65ec77bf8b7b80
orlamee <meetmichaelakindele@gmail.com>
2023-02-10 10:49:23 +0100

    fixes

--- src/components/landingpage/newHero.js
+++ src/components/landingpage/newHero.js
@@ -13,7 +13,7 @@ function NewHero() {
             <h6 className="mb-4">Ardilla helps you save and invest in achieving your financial goals, we have the most Flexible and seamless way<br/>to grow your funds.</h6>
             <Link className='btn btn-outline-primary px-5 py-3 white-btn fs-6' to="/login">Get Started</Link>
             <div className="mt-5">
-              <img src={homepix} alt="banner" className="img-fluid" />
+              <img src={homepix} alt="banner" className="img-fluid" style={{marginBottom: "35px"}} />
             </div>
           </div>
         </div>
--- src/components/landingpage/product.js
+++ src/components/landingpage/product.js
@@ -1,5 +1,6 @@
 import React from "react";
 import checklist from "../../img/checklist.svg";
+import save from "../../img/dilla-pro.svg";
 
 function Products() {
   return(
@@ -9,39 +10,40 @@ function Products() {
           <h2>Products</h2>
         </div>
         <div className="row top-products">
-          <div className="col-md-6 product-scroller" id="style-8">
-            <div className="scrollbar">
+          <div className="col-md-6" id="style-8">
+            <div className="">
               <h3>Save</h3>
               <h6>Save at your own pace!</h6>
-              <p>Save your money daily, weekly, or monthly. No pressure. We work at<br/>your pace</p>
-              <div className="top-products">
-                <div className="d-flex flex-row mb-4">
+              <p>Save your money daily, weekly, or monthly. No pressure. We work at your pace</p>
+              <div className="" style={{marginTop: "60px"}}>
+                <div className="d-flex flex-row mb-5">
                   <img src={checklist} alt="check" className="img-fluid"/>
                   <h5 className="ms-4 mt-1">Create Unlimted savings plans</h5>
                 </div>
-                <div className="d-flex flex-row mb-4">
+                <div className="d-flex flex-row mb-5">
                   <img src={checklist} alt="check" className="img-fluid"/>
                   <h5 className="ms-4 mt-1">Withdraw anytime</h5>
                 </div>
-                <div className="d-flex flex-row mb-4">
+                <div className="d-flex flex-row mb-5">
                   <img src={checklist} alt="check" className="img-fluid"/>
                   <h5 className="ms-4 mt-1">Save with Friends with SAN ID</h5>
                 </div>
-                <div className="d-flex flex-row mb-4">
+                <div className="d-flex flex-row mb-5">
                   <img src={checklist} alt="check" className="img-fluid"/>
                   <h5 className="ms-4 mt-1">Be better at saving</h5>
                 </div>
-                <div className="d-flex flex-row mb-4">
+                <div className="d-flex flex-row mb-5">
                   <img src={checklist} alt="check" className="img-fluid"/>
                   <h5 className="ms-4 mt-1">Accountability profile</h5>
                 </div>
-                <div className="d-flex flex-row mb-4">
+                <div className="d-flex flex-row mb-5">
                   <img src={checklist} alt="check" className="img-fluid"/>
                   <h5 className="ms-4 mt-1">Activity and report</h5>
                 </div>
               </div>
             </div>
-            <div className="scrollbar mt-5">
+            
+            {/* <div className="scrollbar mt-5">
               <h3>Learn</h3>
               <h6>Financial freedom begins with You and Ardilla</h6>
               <p>Ardila offers wealth-building tips from great financial minds to help you<br/>get to where you need to be.</p>
@@ -71,8 +73,8 @@ function Products() {
                   <h5 className="ms-4 mt-1">Referral benefits</h5>
                 </div>
               </div>
-            </div>
-            <div className="scrollbar mt-5">
+            </div> */}
+            {/* <div className="scrollbar mt-5">
               <h3>Invest</h3>
               <h6>Financial freedom begins with You and Ardilla</h6>
               <p>Ardila offers wealth-building tips from great financial minds to help you<br/>get to where you need to be.</p>
@@ -164,10 +166,12 @@ function Products() {
                   <h5 className="ms-4 mt-1">Referral benefits</h5>
                 </div>
               </div>
-            </div>
+            </div> */}
             
           </div>
-          <div className="col-md-6"></div>
+          <div className="col-md-6">
+            <img src={save} alt="savings" className="img-fluid w-100" />
+          </div>
         </div>
         
       </div>
