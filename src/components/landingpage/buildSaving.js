c2586ab338e31d49825c1966ebf11b77e60d2b2d
orlamee <meetmichaelakindele@gmail.com>
2023-03-09 10:05:05 +0100

    homepage

--- src/components/landingpage/buildSaving.js
+++ src/components/landingpage/buildSaving.js
@@ -1,5 +1,6 @@
 import React from "react";
 import calculate from "../../img/calculator.svg"
+import { Link } from "react-router-dom";
 
 
 function Build() {
@@ -8,8 +9,14 @@ function Build() {
       <div className="container">
         <div className="row" data-aos="slide-right" data-aos-duration="1000">
           <div className="col-md-6">
-            <h2 className="mb-3">We’re keen on<br/><span style={{color: "#E8356D"}}>transparency.</span></h2>
-            <h6>Calculate your interests everytime you save on our platform</h6>
+            <h2 className="mb-5">We’re Keen On<br/><span style={{color: "#E8356D"}}>Transparency</span></h2>
+            <h6 className="mb-5">Calculate your interests everytime you save on our platform</h6>
+            <Link
+              className="btn btn-outline-primary px-5 py-3 ardilla-btn fs-6"
+              to="/sign-up"
+            >
+              Get Started
+            </Link>
           </div>
           <div className="col-md-6">
             <img src={calculate} alt="calculator" className="img-fluid w-100" style={{borderRadius: "25px"}} />
--- src/components/landingpage/ddv.jsx
+++ src/components/landingpage/ddv.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import shield from "../../img/draem.svg";
-import dib from "../../img/DIB.svg";
-import vault from "../../img/Vault.svg";
-import fly from "../../img/fly.svg";
+import shield from "../../img/newdreams.svg";
+import dib from "../../img/newdibb.svg";
+import vault from "../../img/newvault.svg";
+import grit from "../../img/grittttttt.svg";
+import fly from "../../img/fly-fff.svg";
 
 
 function DDV() {
@@ -20,36 +21,36 @@ function DDV() {
         <div className="top-products graphbg">
           <div className="row mb-4 justify-content-around">
             <div className="col-md-4 mb-3">
-              <div className="">
+              <div className="ddv-cards">
                 <img src={shield} alt="" className="img-fluid" style={{marginLeft: "-56px"}} />
-                <h1>Dreams</h1>
-                <p className="my-4">Lorem ipsum dolor sit amet consectetur. Felis proin ut proin aliquet semper congue malesuada. Eget penatibus </p>
+                <h1 style={{color: "#F0FDFA"}}>Dreams</h1>
+                <p className="my-4">Helping you make your dreams a reality is important to us. We have helped so many others. We can help you as well. </p>
                 <Link className="text-white">Learn More</Link>
               </div>
             </div>
             <div className="col-md-4 mb-3">
-              <div className="">
+              <div className="ddv-cards">
                 <img src={dib} alt="" className="img-fluid" style={{marginLeft: "-56px"}} />
-                <h1>DIB</h1>
-                <p className="my-4">Lorem ipsum dolor sit amet consectetur. Felis proin ut proin aliquet semper congue malesuada. Eget penatibus </p>
+                <h1 style={{color: "#FEFCE8"}}>DIB</h1>
+                <p className="my-4">Nothing beats an emergency quite like an emergency fund. With Ardilla you can get started building that emergency fund.</p>
                 <Link className="text-white">Learn More</Link>
               </div>
             </div>
           </div>
           <div className="row top-products justify-content-around">
             <div className="col-md-4 mb-3">
-              <div className="">
-                <img src={dib} alt="" className="img-fluid" style={{marginLeft: "-56px"}} />
-                <h1>DIB</h1>
-                <p className="my-4">Lorem ipsum dolor sit amet consectetur. Felis proin ut proin aliquet semper congue malesuada. Eget penatibus </p>
+              <div className="ddv-cards">
+                <img src={vault} alt="" className="img-fluid" style={{marginLeft: "-56px"}} />
+                <h1 style={{color: "#FCE8EE"}}>Vault</h1>
+                <p className="my-4">Lock away excess funds you don't know what to do with. We can help you safeguard it till you are ready. It definitely beats spending it and wondering where all that money went to. </p>
                 <Link className="text-white">Learn More</Link>
               </div>
             </div>
             <div className="col-md-4 mb-3">
-              <div className="">
-                <img src={vault} alt="" className="img-fluid" style={{marginLeft: "-56px"}} />
-                <h1>Vault</h1>
-                <p className="my-4">Lorem ipsum dolor sit amet consectetur. Felis proin ut proin aliquet semper congue malesuada. Eget penatibus </p>
+              <div className="ddv-cards">
+                <img src={grit} alt="" className="img-fluid" style={{marginLeft: "-56px"}} />
+                <h1 style={{color: "#FEFCE8"}}>Grit</h1>
+                <p className="my-4">Become a VIP when you save more money with us. Enjoy better benefits.</p>
                 <Link className="text-white">Learn More</Link>
               </div>
             </div>
