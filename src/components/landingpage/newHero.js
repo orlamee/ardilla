e19b411aea0a193789e5b6016259c2824f6a21ce
orlamee <meetmichaelakindele@gmail.com>
2023-03-09 12:39:45 +0100

    fixes

--- src/components/landingpage/newHero.js
+++ src/components/landingpage/newHero.js
@@ -10,7 +10,7 @@ function NewHero() {
         <div className="row justify-content-center">
           <div className="col-md-10 text-center" >
             <h2>Your Access To More</h2>
-            <h6 className="mb-4">Ardilla helps you save and invest in achieving your financial goals, we have the most flexible and seamless way<br/>to grow your funds.</h6>
+            <h6 className="mb-5">Ardilla helps you save and invest in achieving your financial goals, we have the most flexible and seamless way<br/>to grow your funds.</h6>
             <Link className='btn btn-outline-primary px-5 py-3 white-btn fs-6' to="/login">Get Started</Link>
             <div className="mt-5">
               <img src={homepix} alt="banner" className="img-fluid" style={{marginBottom: "35px"}} />
