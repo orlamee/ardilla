8f96e687b646847a71486642a93d6fba8125c20c
orlamee <meetmichaelakindele@gmail.com>
2023-03-09 15:21:12 +0100

    slider

--- src/components/landingpage/product.js
+++ src/components/landingpage/product.js
@@ -1,6 +1,9 @@
-import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/scrollbar";
 import checklist from "../../img/checklist.svg";
 import save from "../../img/dilla-proo.svg";
+import { Scrollbar } from "swiper";
 
 function Products() {
   return(
@@ -9,44 +12,217 @@ function Products() {
         <div className="row">
           <h2>Products</h2>
         </div>
-        <div className="row justify-content-around" style={{marginTop: "40px"}}>
-          <div className="col-md-5" id="style-8">
-            <div className="">
-              <h3>Save</h3>
-              <h6>Save at your own pace!</h6>
-              <p>Save your money daily, weekly, or monthly. No pressure. We work at your pace</p>
-              <div className="" style={{marginTop: "60px"}}>
-                <div className="d-flex flex-row mb-4">
-                  <img src={checklist} alt="check" className="img-fluid"/>
-                  <h5 className="ms-4 mt-1">Create Unlimted savings plans</h5>
-                </div>
-                <div className="d-flex flex-row mb-4">
-                  <img src={checklist} alt="check" className="img-fluid"/>
-                  <h5 className="ms-4 mt-1">Withdraw anytime</h5>
-                </div>
-                <div className="d-flex flex-row mb-4">
-                  <img src={checklist} alt="check" className="img-fluid"/>
-                  <h5 className="ms-4 mt-1">Save with Friends with SAN ID</h5>
-                </div>
-                <div className="d-flex flex-row mb-4">
-                  <img src={checklist} alt="check" className="img-fluid"/>
-                  <h5 className="ms-4 mt-1">Be better at saving</h5>
-                </div>
-                <div className="d-flex flex-row mb-4">
-                  <img src={checklist} alt="check" className="img-fluid"/>
-                  <h5 className="ms-4 mt-1">Accountability profile</h5>
-                </div>
-                <div className="d-flex flex-row mb-4">
-                  <img src={checklist} alt="check" className="img-fluid"/>
-                  <h5 className="ms-4 mt-1">Activity and report</h5>
+        <Swiper
+          scrollbar={{
+            hide: false, draggable: true,
+          }}
+          modules={[Scrollbar]}
+          className="mySwiper"
+          navigation
+        >
+          <SwiperSlide className="mb-5">
+            <div className="row justify-content-around" style={{marginTop: "40px"}}>
+              <div className="col-md-5" id="style-8">
+                <div className="">
+                  <h3>Save</h3>
+                  <h6>Save at your own pace!</h6>
+                  <p>Save your money daily, weekly, or monthly. No pressure. We work at your pace</p>
+                  <div className="" style={{marginTop: "60px"}}>
+                    <div className="d-flex flex-row mb-4">
+                      <img src={checklist} alt="check" className="img-fluid"/>
+                      <h5 className="ms-4 mt-1">Create Unlimted savings plans</h5>
+                    </div>
+                    <div className="d-flex flex-row mb-4">
+                      <img src={checklist} alt="check" className="img-fluid"/>
+                      <h5 className="ms-4 mt-1">Withdraw anytime</h5>
+                    </div>
+                    <div className="d-flex flex-row mb-4">
+                      <img src={checklist} alt="check" className="img-fluid"/>
+                      <h5 className="ms-4 mt-1">Save with Friends with SAN ID</h5>
+                    </div>
+                    <div className="d-flex flex-row mb-4">
+                      <img src={checklist} alt="check" className="img-fluid"/>
+                      <h5 className="ms-4 mt-1">Be better at saving</h5>
+                    </div>
+                    <div className="d-flex flex-row mb-4">
+                      <img src={checklist} alt="check" className="img-fluid"/>
+                      <h5 className="ms-4 mt-1">Accountability profile</h5>
+                    </div>
+                    <div className="d-flex flex-row mb-4">
+                      <img src={checklist} alt="check" className="img-fluid"/>
+                      <h5 className="ms-4 mt-1">Activity and report</h5>
+                    </div>
+                  </div>
                 </div>
               </div>
+              <div className="col-md-5">
+                <img src={save} alt="savings" className="img-fluid w-100" />
+              </div>
             </div>
-          </div>
-          <div className="col-md-5">
-            <img src={save} alt="savings" className="img-fluid w-100" />
-          </div>
-        </div>
+          </SwiperSlide>
+          <SwiperSlide className="mb-5">
+            <div className="row justify-content-around" style={{marginTop: "40px"}}>
+              <div className="col-md-5" id="style-8">
+                <div className="">
+                  <h3>Save</h3>
+                  <h6>Save at your own pace!</h6>
+                  <p>Save your money daily, weekly, or monthly. No pressure. We work at your pace</p>
+                  <div className="" style={{marginTop: "60px"}}>
+                    <div className="d-flex flex-row mb-4">
+                      <img src={checklist} alt="check" className="img-fluid"/>
+                      <h5 className="ms-4 mt-1">Create Unlimted savings plans</h5>
+                    </div>
+                    <div className="d-flex flex-row mb-4">
+                      <img src={checklist} alt="check" className="img-fluid"/>
+                      <h5 className="ms-4 mt-1">Withdraw anytime</h5>
+                    </div>
+                    <div className="d-flex flex-row mb-4">
+                      <img src={checklist} alt="check" className="img-fluid"/>
+                      <h5 className="ms-4 mt-1">Save with Friends with SAN ID</h5>
+                    </div>
+                    <div className="d-flex flex-row mb-4">
+                      <img src={checklist} alt="check" className="img-fluid"/>
+                      <h5 className="ms-4 mt-1">Be better at saving</h5>
+                    </div>
+                    <div className="d-flex flex-row mb-4">
+                      <img src={checklist} alt="check" className="img-fluid"/>
+                      <h5 className="ms-4 mt-1">Accountability profile</h5>
+                    </div>
+                    <div className="d-flex flex-row mb-4">
+                      <img src={checklist} alt="check" className="img-fluid"/>
+                      <h5 className="ms-4 mt-1">Activity and report</h5>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-5">
+                <img src={save} alt="savings" className="img-fluid w-100" />
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="mb-5">
+            <div className="row justify-content-around" style={{marginTop: "40px"}}>
+              <div className="col-md-5" id="style-8">
+                <div className="">
+                  <h3>Save</h3>
+                  <h6>Save at your own pace!</h6>
+                  <p>Save your money daily, weekly, or monthly. No pressure. We work at your pace</p>
+                  <div className="" style={{marginTop: "60px"}}>
+                    <div className="d-flex flex-row mb-4">
+                      <img src={checklist} alt="check" className="img-fluid"/>
+                      <h5 className="ms-4 mt-1">Create Unlimted savings plans</h5>
+                    </div>
+                    <div className="d-flex flex-row mb-4">
+                      <img src={checklist} alt="check" className="img-fluid"/>
+                      <h5 className="ms-4 mt-1">Withdraw anytime</h5>
+                    </div>
+                    <div className="d-flex flex-row mb-4">
+                      <img src={checklist} alt="check" className="img-fluid"/>
+                      <h5 className="ms-4 mt-1">Save with Friends with SAN ID</h5>
+                    </div>
+                    <div className="d-flex flex-row mb-4">
+                      <img src={checklist} alt="check" className="img-fluid"/>
+                      <h5 className="ms-4 mt-1">Be better at saving</h5>
+                    </div>
+                    <div className="d-flex flex-row mb-4">
+                      <img src={checklist} alt="check" className="img-fluid"/>
+                      <h5 className="ms-4 mt-1">Accountability profile</h5>
+                    </div>
+                    <div className="d-flex flex-row mb-4">
+                      <img src={checklist} alt="check" className="img-fluid"/>
+                      <h5 className="ms-4 mt-1">Activity and report</h5>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-5">
+                <img src={save} alt="savings" className="img-fluid w-100" />
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="mb-5">
+            <div className="row justify-content-around" style={{marginTop: "40px"}}>
+              <div className="col-md-5" id="style-8">
+                <div className="">
+                  <h3>Save</h3>
+                  <h6>Save at your own pace!</h6>
+                  <p>Save your money daily, weekly, or monthly. No pressure. We work at your pace</p>
+                  <div className="" style={{marginTop: "60px"}}>
+                    <div className="d-flex flex-row mb-4">
+                      <img src={checklist} alt="check" className="img-fluid"/>
+                      <h5 className="ms-4 mt-1">Create Unlimted savings plans</h5>
+                    </div>
+                    <div className="d-flex flex-row mb-4">
+                      <img src={checklist} alt="check" className="img-fluid"/>
+                      <h5 className="ms-4 mt-1">Withdraw anytime</h5>
+                    </div>
+                    <div className="d-flex flex-row mb-4">
+                      <img src={checklist} alt="check" className="img-fluid"/>
+                      <h5 className="ms-4 mt-1">Save with Friends with SAN ID</h5>
+                    </div>
+                    <div className="d-flex flex-row mb-4">
+                      <img src={checklist} alt="check" className="img-fluid"/>
+                      <h5 className="ms-4 mt-1">Be better at saving</h5>
+                    </div>
+                    <div className="d-flex flex-row mb-4">
+                      <img src={checklist} alt="check" className="img-fluid"/>
+                      <h5 className="ms-4 mt-1">Accountability profile</h5>
+                    </div>
+                    <div className="d-flex flex-row mb-4">
+                      <img src={checklist} alt="check" className="img-fluid"/>
+                      <h5 className="ms-4 mt-1">Activity and report</h5>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-5">
+                <img src={save} alt="savings" className="img-fluid w-100" />
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className="mb-5">
+            <div className="row justify-content-around" style={{marginTop: "40px"}}>
+              <div className="col-md-5" id="style-8">
+                <div className="">
+                  <h3>Save</h3>
+                  <h6>Save at your own pace!</h6>
+                  <p>Save your money daily, weekly, or monthly. No pressure. We work at your pace</p>
+                  <div className="" style={{marginTop: "60px"}}>
+                    <div className="d-flex flex-row mb-4">
+                      <img src={checklist} alt="check" className="img-fluid"/>
+                      <h5 className="ms-4 mt-1">Create Unlimted savings plans</h5>
+                    </div>
+                    <div className="d-flex flex-row mb-4">
+                      <img src={checklist} alt="check" className="img-fluid"/>
+                      <h5 className="ms-4 mt-1">Withdraw anytime</h5>
+                    </div>
+                    <div className="d-flex flex-row mb-4">
+                      <img src={checklist} alt="check" className="img-fluid"/>
+                      <h5 className="ms-4 mt-1">Save with Friends with SAN ID</h5>
+                    </div>
+                    <div className="d-flex flex-row mb-4">
+                      <img src={checklist} alt="check" className="img-fluid"/>
+                      <h5 className="ms-4 mt-1">Be better at saving</h5>
+                    </div>
+                    <div className="d-flex flex-row mb-4">
+                      <img src={checklist} alt="check" className="img-fluid"/>
+                      <h5 className="ms-4 mt-1">Accountability profile</h5>
+                    </div>
+                    <div className="d-flex flex-row mb-4">
+                      <img src={checklist} alt="check" className="img-fluid"/>
+                      <h5 className="ms-4 mt-1">Activity and report</h5>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-5">
+                <img src={save} alt="savings" className="img-fluid w-100" />
+              </div>
+            </div>
+          </SwiperSlide>
+
+        </Swiper>
+        
         
       </div>
     </section>
