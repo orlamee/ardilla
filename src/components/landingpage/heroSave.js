532e1d3d3123606427ec22f612f95313223cb1f6
orlamee <meetmichaelakindele@gmail.com>
2023-03-13 10:32:07 +0100

    fixes

--- src/components/landingpage/heroSave.js
+++ src/components/landingpage/heroSave.js
@@ -9,7 +9,7 @@ function HeroSave() {
         <div className="row">
           <div className="col-md-6 mb-3 mt-5" >
             <h2 className="desktop">Save with <br/><span style={{color: "#E8356D"}}>Ardilla</span></h2>
-            <h2 className="mobile">Save with <br/>To <span style={{color: "#E8356D"}}>Ardilla</span></h2>
+            <h2 className="mobile">Save with <span style={{color: "#E8356D"}}>Ardilla</span></h2>
             <p className="my-4">Since the beginning of time, saving has been a tried and trusted method <br/>of building wealth. </p>
             <h6>With Ardilla, you can structure your finances better, get financial<br/>security through our saving plans, and lay the groundwork for wealth<br/>building. All while earning more.</h6>
           </div>
