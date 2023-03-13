c2149a4d724609075b84300ca9086d9ed7ebd743
orlamee <meetmichaelakindele@gmail.com>
2023-03-13 17:01:59 +0100

    fixes

--- src/components/dashboardParts/sanLogin.js
+++ src/components/dashboardParts/sanLogin.js
@@ -163,7 +163,7 @@ function SANLogin() {
       )}
       <div className="d-lg-flex half">
         <div className="bg order-1 order-md-2 new-aside"></div>
-        <div className="contents order-2 order-md-1">
+        <div className="contents order-2 order-md-1 bg-white">
           <div className="container">
             <div className="row align-items-center justify-content-center">
               <div className="col-md-7">
