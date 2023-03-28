231cfd92e1347d03eb4b813d3e84c1e0353ae7f4
orlamee <meetmichaelakindele@gmail.com>
2023-03-28 09:43:29 +0100

    fixes

--- src/components/landingpage/perks.js
+++ src/components/landingpage/perks.js
@@ -9,7 +9,7 @@ function Perks() {
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-md-9 text-center">
-            <h2 className="text-white">Besides a competitive salary, we also provide you with the support needed to do your best work, and make your life easier.</h2>
+            <h2 className="text-white">Beyond a competitive salary, we offer so much more in support and necessary tools needed to get the best results.</h2>
           </div>
         </div>
       </div>
@@ -23,35 +23,35 @@ function Perks() {
           <div className="row justify-content-center">
             <div className="col-md-3 mb-3">
               <img src={globe} alt="perks" className="img-fluid" />
-              <h5 className="my-4">Work Anywhere</h5>
-              <p>As long as you do your work</p>
+              <h5 className="my-4">Its a vibe</h5>
+              <p>Our work culture is a vibe and more.</p>
             </div>
             <div className="col-md-3 mb-3">
               <img src={globe} alt="perks" className="img-fluid" />
-              <h5 className="my-4">Work Anywhere</h5>
-              <p>As long as you do your work</p>
+              <h5 className="my-4">Cool gadgets</h5>
+              <p>You get to work with cool new gadgets.</p>
             </div>
             <div className="col-md-3 mb-3">
               <img src={globe} alt="perks" className="img-fluid" />
-              <h5 className="my-4">Work Anywhere</h5>
-              <p>As long as you do your work</p>
+              <h5 className="my-4">Health benefits</h5>
+              <p>We know health is important. So we provided easy access and affordable health benefits.</p>
             </div>
           </div>
           <div className="row justify-content-center mt-4">
             <div className="col-md-3 mb-3">
               <img src={globe} alt="perks" className="img-fluid" />
-              <h5 className="my-4">Work Anywhere</h5>
-              <p>As long as you do your work</p>
+              <h5 className="my-4">Team bonding</h5>
+              <p>We always make out time for fun and getting to know your team mates better.</p>
             </div>
             <div className="col-md-3 mb-3">
               <img src={globe} alt="perks" className="img-fluid" />
-              <h5 className="my-4">Work Anywhere</h5>
-              <p>As long as you do your work</p>
+              <h5 className="my-4">Diversity</h5>
+              <p>We promote diversity of ideas and people. Anyone from anywhere is welcome.</p>
             </div>
             <div className="col-md-3 mb-3">
               <img src={globe} alt="perks" className="img-fluid" />
-              <h5 className="my-4">Work Anywhere</h5>
-              <p>As long as you do your work</p>
+              <h5 className="my-4">Education and career progression</h5>
+              <p>We offer you opportunities to grow, learn, and advance in your career.</p>
             </div>
           </div>
         </div>
