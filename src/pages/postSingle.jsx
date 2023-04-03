203f59012e2404b54629af4b28a1413fb40a2408
orlamee <meetmichaelakindele@gmail.com>
2023-04-03 12:41:50 +0100

    Blog

--- src/pages/postSingle.jsx
+++ src/pages/postSingle.jsx
@@ -1,13 +1,200 @@
+import blog from "../img/blog-img.png";
+import two from "../img/part.svg";
+import adv from "../img/advertising.svg";
+
+
 function PostSingle() {
   return (
     <section className="postheader">
       <div className="container">
         <div className="row">
           <div className="col-md-9">
-            <span className="bg-test">Savings</span>
-            <h2>Saving with Ardilla changed my financial life entirely</h2>
+            <div className="d-flex flex-row">
+              <span className="bg-test">Savings</span>
+              <i className="bi bi-link-45deg mx-5 fs-4"></i>
+              <i className="bi bi-share-fill fs-4"></i>
+            </div>
+            <h2 className="mt-2">Saving with Ardilla changed my financial life entirely</h2>
+            <div className="row mt-3">
+              <div className="col-md-6">
+                <div className="d-flex flex-row">
+                  <h5>By <span style={{color: "#3D0072"}}>Olukoju Matthew</span></h5>
+                  <h5 className="mx-4">August 5, 2023</h5>
+                  <h5>5min read</h5>
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="d-flex flex-row">
+                  <i className="bi bi-facebook me-4"></i>
+                  <i className="bi bi-linkedin me-4"></i>
+                  <i className="bi bi-twitter me-4"></i>
+                  <i className="bi bi-whatsapp me-4"></i>
+                  <i className="bi bi-envelope-fill"></i>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
+        <div className="row my-5">
+          <div className="col">
+            <img src={blog} alt="blog-img" className="img-fluid w-100" />
+          </div>
+        </div>
+        <div className="row toc">
+          <div className="col-md-8">
+            <h4 className="mb-4">Table of content</h4>
+            <ol>
+              <li>What is an Emergency Fund</li>
+              <li>What is an Emergency Fund</li>
+              <li>What is an Emergency Fund</li>
+              <li>What is an Emergency Fund</li>
+              <li>What is an Emergency Fund</li>
+              <li>What is an Emergency Fund</li>
+            </ol>
+            <div className="mb-5">
+              <h6>How did you find out about Ardilla and how long have<br/>you been using the app?</h6>
+              <span>As risk takers, their aim is to exploit price fluctuations in the stock market by making high-risk investments </span>
+            </div>
+            <div className="mb-5">
+              <h6>How did you find out about Ardilla and how long have<br/>you been using the app?</h6>
+              <span>As risk takers, their aim is to exploit price fluctuations in the stock market by making high-risk investments </span>
+            </div>
+            <div className="mb-5">
+              <h6>How did you find out about Ardilla and how long have<br/>you been using the app?</h6>
+              <span>As risk takers, their aim is to exploit price fluctuations in the stock market by making high-risk investments </span>
+            </div>
+            <div className="mb-5">
+              <h6>How did you find out about Ardilla and how long have<br/>you been using the app?</h6>
+              <span>As risk takers, their aim is to exploit price fluctuations in the stock market by making high-risk investments </span>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <h4 className="mb-4" style={{color: "#3D0072"}}>Popular Articles</h4>
+            <div className="mb-4">
+              <img src={two} alt="testimonial" className="img-fluid w-100 mb-3" />
+              <span className="bg-test">Updates</span>
+              <h3 className="my-3">Saving with Ardilla changed my financial life entirely</h3>
+              <div className="">
+                <p>By <span style={{color:"#3D0072"}}>Olukoju Matthew</span></p>
+                <p className="float-end auth-date">August 5, 2023</p>
+              </div>
+            </div>
+            <div className="mb-4">
+              <img src={two} alt="testimonial" className="img-fluid w-100 mb-3" />
+              <span className="bg-test">Guides</span>
+              <h3 className="my-3">Saving with Ardilla changed my financial life entirely</h3>
+              <div className="">
+                <p>By <span style={{color:"#3D0072"}}>Olukoju Matthew</span></p>
+                <p className="float-end auth-date">August 5, 2023</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="advert my-5">
+          <div className="row">
+            <div className="col-md-8" style={{marginTop: "73px"}}>
+              <h4>Online Savings that has helped alot of Nigerians</h4>
+              <span className="mt-3">Jan 1, 2023</span>
+            </div>
+            <div className="col-md-4">
+              <img src={two} alt="" className="img-fluid" />
+            </div>
+          </div>
+        </div>
+        <div className="row toc">
+          <div className="col-md-10">
+            <div className="mb-5">
+              <h6>How has Ardilla helped you in your financial life since you learnt about them</h6>
+              <span>Hello! This Privacy Policy (the “Policy”) has been created to describe your privacy rights regarding how we collect, use, process, disclose and protect your information, including any non-public, personal information (Personal Data).
+              This Policy applies to the Personal Data collected by Ardilla when you use our website, mobile application, social media platforms, tools, and any other platform belonging to Ardilla (collectively, the "Services") or otherwise interact with us as described below. This Policy may be updated occasionally and at our sole discretion.  </span>
+            </div>
+            <div className="mb-5">
+              <h6>How has Ardilla helped you in your financial life since you learnt about them</h6>
+              <span>Hello! This Privacy Policy (the “Policy”) has been created to describe your privacy rights regarding how we collect, use, process, disclose and protect your information, including any non-public, personal information (Personal Data).
+              This Policy applies to the Personal Data collected by Ardilla when you use our website, mobile application, social media platforms, tools, and any other platform belonging to Ardilla (collectively, the "Services") or otherwise interact with us as described below. This Policy may be updated occasionally and at our sole discretion.  </span>
+            </div>
+            <div className="mb-5">
+              <h6>How has Ardilla helped you in your financial life since you learnt about them</h6>
+              <span>Hello! This Privacy Policy (the “Policy”) has been created to describe your privacy rights regarding how we collect, use, process, disclose and protect your information, including any non-public, personal information (Personal Data).
+              This Policy applies to the Personal Data collected by Ardilla when you use our website, mobile application, social media platforms, tools, and any other platform belonging to Ardilla (collectively, the "Services") or otherwise interact with us as described below. This Policy may be updated occasionally and at our sole discretion.  </span>
+            </div>
+            
+          </div>
+        </div>
+        <div className="row my-5">
+          <div className="col">
+            <img src={adv} alt="blog-img" className="img-fluid w-100" />
+          </div>
+        </div>
+        <div className="row toc">
+          <div className="col-md-10">
+            <div className="mb-5">
+              <h6>How has Ardilla helped you in your financial life since you learnt about them</h6>
+              <span>Hello! This Privacy Policy (the “Policy”) has been created to describe your privacy rights regarding how we collect, use, process, disclose and protect your information, including any non-public, personal information (Personal Data).
+              This Policy applies to the Personal Data collected by Ardilla when you use our website, mobile application, social media platforms, tools, and any other platform belonging to Ardilla (collectively, the "Services") or otherwise interact with us as described below. This Policy may be updated occasionally and at our sole discretion.  </span>
+            </div>
+            <div className="mb-5">
+              <h6>How has Ardilla helped you in your financial life since you learnt about them</h6>
+              <span>Hello! This Privacy Policy (the “Policy”) has been created to describe your privacy rights regarding how we collect, use, process, disclose and protect your information, including any non-public, personal information (Personal Data).
+              This Policy applies to the Personal Data collected by Ardilla when you use our website, mobile application, social media platforms, tools, and any other platform belonging to Ardilla (collectively, the "Services") or otherwise interact with us as described below. This Policy may be updated occasionally and at our sole discretion.  </span>
+            </div>
+            <div className="mb-5">
+              <h6>How has Ardilla helped you in your financial life since you learnt about them</h6>
+              <span>Hello! This Privacy Policy (the “Policy”) has been created to describe your privacy rights regarding how we collect, use, process, disclose and protect your information, including any non-public, personal information (Personal Data).
+              This Policy applies to the Personal Data collected by Ardilla when you use our website, mobile application, social media platforms, tools, and any other platform belonging to Ardilla (collectively, the "Services") or otherwise interact with us as described below. This Policy may be updated occasionally and at our sole discretion.  </span>
+            </div>
+            
+          </div>
+        </div>
+        <div className="advert my-5">
+          <div className="row">
+            <div className="col-md-8" style={{marginTop: "73px"}}>
+              <h4>Online Savings that has helped alot of Nigerians</h4>
+              <span className="mt-3">Jan 1, 2023</span>
+            </div>
+            <div className="col-md-4">
+              <img src={two} alt="" className="img-fluid" />
+            </div>
+          </div>
+        </div>
+        <div className="row toc">
+          <div className="col-md-10">
+            <div className="mb-5">
+              <h6>How has Ardilla helped you in your financial life since you learnt about them</h6>
+              <span>Hello! This Privacy Policy (the “Policy”) has been created to describe your privacy rights regarding how we collect, use, process, disclose and protect your information, including any non-public, personal information (Personal Data).
+              This Policy applies to the Personal Data collected by Ardilla when you use our website, mobile application, social media platforms, tools, and any other platform belonging to Ardilla (collectively, the "Services") or otherwise interact with us as described below. This Policy may be updated occasionally and at our sole discretion.  </span>
+            </div>
+            <div className="mb-5">
+              <h6>How has Ardilla helped you in your financial life since you learnt about them</h6>
+              <span>Hello! This Privacy Policy (the “Policy”) has been created to describe your privacy rights regarding how we collect, use, process, disclose and protect your information, including any non-public, personal information (Personal Data).
+              This Policy applies to the Personal Data collected by Ardilla when you use our website, mobile application, social media platforms, tools, and any other platform belonging to Ardilla (collectively, the "Services") or otherwise interact with us as described below. This Policy may be updated occasionally and at our sole discretion.  </span>
+            </div>
+            <div className="mb-5">
+              <h6>How has Ardilla helped you in your financial life since you learnt about them</h6>
+              <span>Hello! This Privacy Policy (the “Policy”) has been created to describe your privacy rights regarding how we collect, use, process, disclose and protect your information, including any non-public, personal information (Personal Data).
+              This Policy applies to the Personal Data collected by Ardilla when you use our website, mobile application, social media platforms, tools, and any other platform belonging to Ardilla (collectively, the "Services") or otherwise interact with us as described below. This Policy may be updated occasionally and at our sole discretion.  </span>
+            </div>
+            
+          </div>
+        </div>
+        <div className="mb-5">
+          <div className="row justify-content-center">
+            <div className="col-md-8 text-center">
+              <div className="bg-subscr">
+                <h4 className="mb-4 text-white">Get More Testimonials</h4>
+                <p className="text-white">Put in your email to get more testimonials</p>
+                <div className="subcribe-form mt-5">
+                  <form id="subscribe" novalidate="true">
+                    <input class="enteremail" name="EMAIL" id="subscribe-email" placeholder="Enter your email" spellcheck="false" type="text"/>
+                    <button type="submit" id="subscribe-button" class="subscribe-button color-bg">Sign up</button>
+                    <label for="subscribe-email" class="subscribe-message"></label>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr />
       </div>
     </section>
   );
