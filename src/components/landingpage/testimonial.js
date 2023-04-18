3a6e7062ce1185ddccbec4b790170b33414837c5
orlamee <meetmichaelakindele@gmail.com>
2023-04-18 12:03:05 +0100

    fixes

--- src/components/landingpage/testimonial.js
+++ src/components/landingpage/testimonial.js
@@ -21,13 +21,13 @@ function Testimonial() {
           <div className="col-md-4 mb-3">
             <div className="traction-cards risky">
               <p>Risk <span className="hgl" style={{background: "#35298A"}}>Management</span></p>
-              <h3>0% Risk</h3>
+              <h3><CountUp end={0} start={10} delay={3} duration="20" suffix="%"  /> Risk</h3>
             </div>
           </div>
           <div className="col-md-4 mb-3">
             <div className="traction-cards paidback">
               <p>Investment <span className="hgl" style={{background: "#9F9704"}}>paid back</span></p>
-              <h3>₦ <CountUp end={250} delay={3} duration={20}  /> M</h3>
+              <h3>₦ <CountUp end={250} delay={3} duration="20"  /> M</h3>
             </div>
           </div>
         </div>
